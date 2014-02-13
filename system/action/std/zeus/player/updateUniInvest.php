b58593bcd3b08f48228b353041141bd9b3a72a08
Jacky Casas <jack_ett8@hotmail.com>
2014-02-13 14:59:29 +0100

    little fix

--- system/action/std/zeus/player/updateUniInvest.php
+++ system/action/std/zeus/player/updateUniInvest.php
@@ -4,13 +4,6 @@ include_once ATHENA;
 
 # int credit 		nouveau montant à investir
 
-if (CTR::$get->exist('baseid')) {
-	$baseId = CTR::$get->get('baseid');
-} elseif (CTR::$post->exist('baseid')) {
-	$baseId = CTR::$post->get('baseid');
-} else {
-	$baseId = FALSE;
-}
 if (CTR::$get->exist('credit')) {
 	$credit = CTR::$get->get('credit');
 } elseif (CTR::$post->exist('credit')) {
@@ -18,19 +11,12 @@ if (CTR::$get->exist('credit')) {
 } else {
 	$credit = FALSE;
 }
-if (CTR::$get->exist('category')) {
-	$category = CTR::$get->get('category');
-} elseif (CTR::$post->exist('category')) {
-	$category = CTR::$post->get('category');
-} else {
-	$category = FALSE;
-}
 
 if ($credit !== FALSE) { 
 	$S_PAM1 = ASM::$pam->getCurrentSession();
 	ASM::$pam->newSession();
 	ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
-	ASM::$pam->get()->iUniversity = $description;
+	ASM::$pam->get()->iUniversity = $credit;
 
 	CTR::$alert->add('L\'investissement dans l\'université a été modifié', ALERT_STD_SUCCESS);
 
