b0ad5d5077a64b82c78af1018ad186acf6caafa7
abdelaz3r <gil.clavien@gmail.com>
2014-07-10 13:42:36 +0200

    fix some bugs

--- system/modules/athena/main.conf.php
+++ system/modules/athena/main.conf.php
@@ -1,38 +1,37 @@
 <?php
-
-// routes commerciales
+# routes commerciales
 define('CRM_COEFROUTEPRICE',		28);
 define('CRM_COEFROUTEINCOME',		0.2);
-define('CRM_CANCELROUTE',			0.8);	// 80% de crédits rendu si on annule la route avant qu'elle soit acceptée
+define('CRM_CANCELROUTE',			0.8);	# 80% de crédits rendu si on annule la route avant qu'elle soit acceptée
 define('CRM_ROUTEBONUSSECTOR', 		1.1);
 define('CRM_ROUTEBONUSCOLOR', 		1.4);
 define('CRM_COEFEXPERIENCE', 		0.1);
 
-// queue de construction de bâtiments
+# queue de construction de bâtiments
 define('BQM_MAXQUEUE', 				4);
 
-// pourcentage de ressources rendues lors
-// de l'annulation d'une construciton de batiment
-define('BQM_RESOURCERETURN', 		0.5); 	// 50%
+# pourcentage de ressources rendues lors
+# de l'annulation d'une construciton de batiment
+define('BQM_RESOURCERETURN', 		0.5); 	# 50%
 
-// queue de construction de vaisseaux
+# queue de construction de vaisseaux
 define('SQM_SHIPMAXQUEUE', 			5);
-define('SQM_RESOURCERETURN',		0.5);	// 50%
+define('SQM_RESOURCERETURN',		0.5);	# 50%
 
-// commandant
-define('MAXCOMMANDERINSCHOOL',		10); // --> à vérifier si on utilise toujours
+# commandant
+define('MAXCOMMANDERINSCHOOL',		10); # --> à vérifier si on utilise toujours
 
-// commercialRoute statement
+# commercialRoute statement
 define('CRM_PROPOSED',				0);
 define('CRM_ACTIVE',				1);
 define('CRM_STANDBY',				2);
 
-// production/storage mode de la raffinerie
+# production/storage mode de la raffinerie
 define('OBM_COEFPRODUCTION', 		0.1);
 
-// limite du stock de ressources pour pouvoir en envoyer
-define('OBM_STOCKLIMIT', 			0);	// 40%
+# limite du stock de ressources pour pouvoir en envoyer
+define('OBM_STOCKLIMIT', 			0);	# 40%
 
-// type of base constants
-define('OBM_LEVEL_MIN_TO_CHANGE_TYPE', 10); // minimal generator level
-define('OBM_LEVEL_MIN_FOR_CAPITAL', 20); // minimal generator level to build a capital base
+# type of base constants
+define('OBM_LEVEL_MIN_TO_CHANGE_TYPE', 10); # minimal generator level
+define('OBM_LEVEL_MIN_FOR_CAPITAL', 20); # minimal generator level to build a capital base
--- system/modules/zeus/main.conf.php
+++ system/modules/zeus/main.conf.php
@@ -1,14 +1,14 @@
 <?php
-// player
+# player
 define('PAM_BASELVLPLAYER',			1250);
 define('PAM_BASELVLCMD',			100);
 define('PAM_BASEAP',				10);
 define('PAM_COEFFAP',				5);
 
-// impot planete
+# impot planete
 define('PAM_COEFTAX',				1);
 
-// player statements
+# player statements
 define('PAM_ACTIVE', 				1);
 define('PAM_INACTIVE',				2);
 define('PAM_HOLIDAY',				3);
@@ -16,7 +16,7 @@ define('PAM_BANNED',				4);
 define('PAM_DELETED',				5);
 define('PAM_DEAD',					6);
 
-// player status
+# player status
 define('PAM_STANDARD',				1);
 define('PAM_PARLIAMENT',			2);
 define('PAM_GOVERNMENT',			3);
