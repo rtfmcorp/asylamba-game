211c6e5fc3b61bd5667b80fbcbf181dae6a81578
Jacky Casas <jack_ett8@hotmail.com>
2014-07-29 18:09:37 +0200

    update script to create the tables for rankings

--- system/script/scripts/database/addRankings.php
+++ system/script/scripts/database/addRankings.php
@@ -2,23 +2,25 @@
 echo '<h1>Ajout de la table userRanking</h1>';
 
 $db = DataBaseAdmin::getInstance();
+$qr = $db->prepare("DROP TABLE IF EXISTS `playerRanking`");
+$qr->execute();
 $qr = $db->prepare("CREATE TABLE IF NOT EXISTS `playerRanking` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rRanking` int(11) NOT NULL,
   `rPlayer` int(11) NOT NULL,
-  `general` smallint(6) NOT NULL,
+  `general` int(11) NOT NULL,
   `generalPosition` smallint(6) NOT NULL,
   `generalVariation` smallint(6) NOT NULL,
-  `experience` smallint(6) NOT NULL,
+  `experience` int(11) NOT NULL,
   `experiencePosition` smallint(6) NOT NULL,
   `experienceVariation` smallint(6) NOT NULL,
-  `victory` smallint(6) NOT NULL,
+  `victory` int(11) NOT NULL,
   `victoryPosition` smallint(6) NOT NULL,
   `victoryVariation` smallint(6) NOT NULL,
-  `defeat` smallint(6) NOT NULL,
+  `defeat` int(11) NOT NULL,
   `defeatPosition` smallint(6) NOT NULL,
   `defeatVariation` smallint(6) NOT NULL,
-  `ratio` smallint(6) NOT NULL,
+  `ratio` int(11) NOT NULL,
   `ratioPosition` smallint(6) NOT NULL,
   `ratioVariation` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
@@ -28,17 +30,19 @@ $qr->execute();
 
 echo '<h1>Ajout de la table factionRanking</h1>';
 
+$qr = $db->prepare("DROP TABLE IF EXISTS `factionRanking`");
+$qr->execute();
 $qr = $db->prepare("CREATE TABLE IF NOT EXISTS `factionRanking` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rRanking` int(11) NOT NULL,
   `rFaction` int(11) NOT NULL,
-  `general` smallint(6) NOT NULL,
+  `general` int(11) NOT NULL,
   `generalPosition` smallint(6) NOT NULL,
   `generalVariation` smallint(6) NOT NULL,
-  `power` smallint(6) NOT NULL,
+  `power` int(11) NOT NULL,
   `powerPosition` smallint(6) NOT NULL,
   `powerVariation` smallint(6) NOT NULL,
-  `domination` smallint(6) NOT NULL,
+  `domination` int(11) NOT NULL,
   `dominationPosition` smallint(6) NOT NULL,
   `dominationVariation` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
