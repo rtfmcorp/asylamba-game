8f60f7216ee91abf92bb63113449df18b85a8db1
noe <noe.zufferey@gmail.com>
2014-08-13 10:17:48 +0200

    fix bug creation base

--- system/inscription/step/ally.php
+++ system/inscription/step/ally.php
@@ -23,7 +23,7 @@ echo '<div id="content">';
 		echo '</div>';
 	echo '</div>';
 
-#	$allies = array(1, 2, 3, 4, 5, 6, 7);
+	# $allies = array(1, 2, 3, 4, 5, 6, 7);
 	$allies = array(2, 3);
 	shuffle($allies);
 
