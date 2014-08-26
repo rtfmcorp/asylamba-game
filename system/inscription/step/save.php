8ac03cab2dfa6a83077fb3dabb360a02239691ed
acknowledge <jack_ett8@hotmail.com>
2014-08-26 18:24:38 +0200

    send a welcome message from Jean-Mi after inscription

--- system/inscription/step/save.php
+++ system/inscription/step/save.php
@@ -4,6 +4,7 @@ include_once ATHENA;
 include_once ZEUS;
 include_once PROMETHEE;
 include_once ARES;
+include_once HERMES;
 
 try {
 	$faction = CTR::$data->get('inscription')->get('ally');
@@ -172,6 +173,29 @@ try {
 
 	include_once GAIA;
 	GalaxyColorManager::apply();
+
+	# send welcome message with Jean-Mi
+	$message = 'Salut,  
+		<br /><br />Moi c\'est Jean-Mi, l\'opérateur du jeu. 
+		<br />Je te souhaite la bienvenue sur Asylamba et espère que tu t\'y plairas.
+		<br />Je t\'enverrai des messages quand tu devras être au courant de choses importantes au fur et à mesure du temps.
+		<br /><br />Bon jeu et à bientôt j\'espère.
+		<br /><br />Cordialement, <br />Jean-Mi';
+	$m = new Message();
+	$m->setRPlayerWriter(0);
+	$m->setDSending(Utils::now());
+	$m->setContent($message);
+
+	$db = DataBase::getInstance();
+	$qr = $db->prepare('SELECT MAX(thread) AS maxThread FROM message');
+	$qr->execute();
+	if ($aw = $qr->fetch()) {
+		$m->setThread($aw['maxThread'] + 1);
+		$m->setRPlayerReader($pl->getId());
+		ASM::$msm->add($m);
+	} else {
+		CTR::$alert->add('Création du message d\'accueil raté :-(. Bienvenue quand même !', ALERT_STD_ERROR);
+	}
 
 	# redirection vers connection
 	CTR::redirect('connection/bindkey-' . $pl->getBind() . '/mode-splash');
