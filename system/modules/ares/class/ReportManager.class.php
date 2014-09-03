4d8fa88d6418d68aa66015d16d944d98e4088d69
noe <noe.zufferey@gmail.com>
2014-09-03 17:24:23 +0200

    loadOnlyReport

--- system/modules/ares/class/ReportManager.class.php
+++ system/modules/ares/class/ReportManager.class.php
@@ -125,6 +125,72 @@ class ReportManager extends Manager {
 		}
 	}
 
+	public function loadOnlyReport($where = array(), $order = array(), $limit = array()) {
+		$formatWhere = Utils::arrayToWhere($where);
+		$formatOrder = Utils::arrayToOrder($order);
+		$formatLimit = Utils::arrayToLimit($limit);
+
+		$db = DataBase::getInstance();
+		$qr = $db->prepare('SELECT r.*,
+			FROM report AS r
+			' . $formatWhere .'
+			' . $formatOrder .'
+			' . $formatLimit
+		);
+
+		foreach($where AS $v) {
+			if (is_array($v)) {
+				foreach ($v as $p) {
+					$valuesArray[] = $p;
+				}
+			} else {
+				$valuesArray[] = $v;
+			}
+		}
+		
+		if (empty($valuesArray)) {
+			$qr->execute();
+		} else {
+			$qr->execute($valuesArray);
+		}
+
+		$awReports = $qr->fetchAll();
+		$qr->closeCursor();
+
+		foreach ($awReports AS $awReport) {
+			$report = new Report();
+
+			$report->id = $awReport['id'];
+			$report->rPlayerAttacker = $awReport['rPlayerAttacker'];
+			$report->rPlayerDefender = $awReport['rPlayerDefender'];
+			$report->rPlayerWinner = $awReport['rPlayerWinner'];
+			$report->avatarA = $awReport['avatarA'];
+			$report->avatarD = $awReport['avatarD'];
+			$report->nameA = $awReport['nameA'];
+			$report->nameD = $awReport['nameD'];
+			$report->levelA = $awReport['levelA'];
+			$report->levelD = $awReport['levelD'];
+			$report->experienceA = $awReport['experienceA'];
+			$report->experienceD = $awReport['experienceD'];
+			$report->palmaresA = $awReport['palmaresA'];
+			$report->palmaresD = $awReport['palmaresD'];
+			$report->resources = $awReport['resources'];
+			$report->expCom = $awReport['expCom'];
+			$report->expPlayerA = $awReport['expPlayerA'];
+			$report->expPlayerD = $awReport['expPlayerD'];
+			$report->rPlace = $awReport['rPlace'];
+			$report->placeName = $awReport['placeName'];
+			$report->type = $awReport['type'];
+			$report->round = $awReport['round'];
+			$report->importance = $awReport['importance'];
+			$report->statementAttacker = $awReport['statementAttacker'];
+			$report->statementDefender = $awReport['statementDefender'];
+			$report->dFight = $awReport['dFight'];
+
+			$this->_Add($report);
+		}
+	}
+
 	public function save() {
 		$db = DataBase::getInstance();
 		$reports = $this->_Save();
