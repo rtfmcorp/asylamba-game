31e25be087628134bd4aa95f9e8926b7a5fe6e94
noe <noe.zufferey@gmail.com>
2014-10-11 14:31:11 +0200

    action for laws module

---
+++ system/action/std/demeter/law/vote.php
@@ -0,0 +1,47 @@
+<?php
+#rlaw	id de la loi
+#vote le vote du joueur
+
+$rLaw = Utils::getHTTPData('rlaw');
+$vote = Utils::getHTTPData('vote');
+
+include_once DEMETER;
+include_once ZEUS;
+
+if ($rLaw !== FALSE && $vote != FALSE) {
+	if (CTR::$data->get('playerInfo')->get('status') > PAM_STANDARD) {
+		$_LAM = ASM::$lam->getCurrentSession();
+		ASM::$lam->newSession();
+		ASM::$lam->load(array('id' => $rLaw));
+
+		if (ASM::$lam->size() > 0) {
+			if (ASM::$lam->get()->statement == Law::VOTATION) {
+				$_VLM = ASM::$vlm->getCurrentSession();
+				ASM::$vlm->newSession();
+				ASM::$vlm->load(array('rPlayer' => CTR::$data->get('playerId'), 'rLaw' => $rLaw));
+
+				if (ASM::$vlm->get() == 0) {
+					$vote = new VoteLaw();
+					$vote->rPlayer = CTR::$data->get('playerId');
+					$vote->rLaw = $rLaw;
+					$vote->vote = $vote;
+					$vote->dCreation = Utils::now();
+					ASM::$vlm->add($vote);
+				} else {
+					CTR::$alert->add('Vous avez déjà voté.', ALERT_STD_ERROR);
+				}
+			} else {
+				CTR::$alert->add('Cette loi est déjà votée.', ALERT_STD_ERROR);
+			}
+			ASM::$vlm->changeSession($_VLM);
+		} else {
+			CTR::$alert->add('Cette loi n\'existe pas.', ALERT_STD_ERROR);
+		}
+
+		ASM::$cam->changeSession($_LAM);
+	} else {
+		CTR::$alert->add('Vous n\'avez pas le droit de voter.', ALERT_STD_ERROR);
+	}
+} else {
+	CTR::$alert->add('Informations manquantes.', ALERT_STD_ERROR);
+}
