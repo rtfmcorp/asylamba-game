fb2d770d14b9b89ef83aed4fcd9f4f7de2c1ea58
abdelaz3r <gil.clavien@gmail.com>
2014-10-15 23:50:16 +0200

    add wysiwig UI in every component

--- system/views/components/faction/forum/topics.php
+++ system/views/components/faction/forum/topics.php
@@ -8,7 +8,7 @@
 	# int 			forum_topics
 	# [{topic}] 	topic_topics
 
-echo '<div class="component report topic">';
+echo '<div class="component">';
 	echo '<div class="head skin-4">';
 		echo '<img class="main" alt="ressource" src="' . MEDIA . 'orbitalbase/situation.png">';
 		echo '<h2>' . ForumResources::getInfoForId($forum_topics, 'name') . '</h2>';
@@ -16,19 +16,25 @@ echo '<div class="component report topic">';
 	echo '</div>';
 	echo '<div class="fix-body">';
 		echo '<div class="body">';
-			echo '<div class="tool">';
-				echo '<span><a href="' . APP_ROOT . 'faction/view-forum/forum-' . $forum_topics . '/mode-create/sftr-2">créer un nouveau sujet</a></span>';
-			echo '</div>';
+			echo '<div class="set-item">';
+				echo '<a class="item" href="' . APP_ROOT . 'faction/view-forum/forum-' . $forum_topics . '/mode-create/sftr-2">';
+					echo '<div class="left">';
+						echo '<span>+</span>';
+					echo '</div>';
 
-			if (count($topic_topics) == 0) {
-				echo '<p class="info">Aucun sujet n\'a encore été créé dans cette partie de forum.</p>';
-			} else {
-				echo '<div class="set-item">';
+					echo '<div class="center">';
+						echo 'Créer un nouveau sujet';
+					echo '</div>';
+				echo '</a>';
+			
+				if (count($topic_topics) == 0) {
+					echo '<p class="info">Aucun sujet n\'a encore été créé dans cette partie de forum.</p>';
+				} else {
 					foreach ($topic_topics as $t) {
 						if ($t->id == CTR::$get->get('topic')) {
 							$isNew = '';
 						} elseif ($t->lastView == NULL || strtotime($t->lastView) < strtotime($t->dLastMessage)) {
-							$isNew = ' new';
+							$isNew = ' round-color' . CTR::$data->get('playerInfo')->get('color');
 						} else {
 							$isNew = '';
 						}
@@ -38,9 +44,9 @@ echo '<div class="component report topic">';
 							echo '<span class="button hb lt" title="message">' . $t->nbMessage . '</span>';
 						echo '</a>';*/
 
-						echo '<div class="item ' . $isNew . '">';
+						echo '<div class="item">';
 							echo '<div class="left">';
-								echo '<span>' . $t->nbMessage . '</span>';
+								echo '<span class="' . $isNew . '">' . $t->nbMessage . '</span>';
 							echo '</div>';
 
 							echo '<div class="center">';
@@ -52,8 +58,8 @@ echo '<div class="component report topic">';
 							echo '</div>';
 						echo '</div>';
 					}
-				echo '</div>';
-			}
+				}
+			echo '</div>';
 		echo '</div>';
 	echo '</div>';
 echo '</div>';
