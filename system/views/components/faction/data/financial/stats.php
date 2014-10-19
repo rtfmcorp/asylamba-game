61afdc75b1ffbe7b143d2d571693fcf589558bba
abdelaz3r <gil.clavien@gmail.com>
2014-10-19 23:35:22 +0200

    add credit evolution in register view

--- system/views/components/faction/data/financial/stats.php
+++ system/views/components/faction/data/financial/stats.php
@@ -1,4 +1,16 @@
 <?php
+include_once ATLAS;
+
+$S_FRM1 = ASM::$frm->getCurrentSession();
+ASM::$frm->newSession();
+ASM::$frm->loadByRequest(
+	'WHERE rFaction = ? ORDER BY rRanking DESC LIMIT 0, 20',
+	array($faction->id)
+);
+
+$creditBase  = ASM::$frm->get(0)->general;
+$creditBase += $creditBase * 12 / 100;
+
 echo '<div class="component profil">';
 	echo '<div class="head skin-2">';
 		echo '<h2>Finance</h2>';
@@ -8,15 +20,30 @@ echo '<div class="component profil">';
 			echo '<h4>Richesse</h4>';
 
 			echo '<div class="number-box half grey">';
-				echo '<span class="label">Richesse de la faction</span>';
+				echo '<span class="label">Fortune de la faction</span>';
 				echo '<span class="value">';
 					echo Format::number($faction->credits);
 					echo ' <img class="icon-color" src="' . MEDIA . 'resources/credit.png" alt="crédits" />';
 				echo '</span>';
 			echo '</div>';
 
-			echo '<p>évolution des crédits (20 derniers jours)</p>';
+
+			echo '<div class="evolution">';
+				echo '<div class="header">Evolution de la fortune de la faction sur les 20 derniers segments.</div>';
+				echo '<div class="body">';
+				for ($i = 0; $i < ASM::$frm->size(); $i++) {
+					echo '<span class="progress-bar">';
+						echo '<span style="width:' . Format::percent(ASM::$frm->get($i)->general, $creditBase) . '%;" class="content">';
+							echo Format::number(ASM::$frm->get($i)->general);
+						echo '</span>';
+					echo '</span>';
+				}
+				echo '</div>';
+			echo '</div>';
+
 		echo '</div>';
 	echo '</div>';
 echo '</div>';
+
+ASM::$frm->changeSession($S_FRM1);
 ?>
