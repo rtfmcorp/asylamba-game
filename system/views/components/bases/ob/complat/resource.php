545883796e4e8788c8c758ba4663a01c1efeb8f4
abdelaz3r <gil.clavien@gmail.com>
2014-11-26 17:36:25 +0100

    amélioration : refonte légère de l'interface d'envoi de ressources

--- system/views/components/bases/ob/complat/resource.php
+++ system/views/components/bases/ob/complat/resource.php
@@ -1,5 +1,5 @@
 <?php
-echo '<div class="component new-message">';
+echo '<div class="component new-message market-sell">';
 	echo '<div class="head skin-4 sh">';
 		echo '<img src="' . MEDIA . 'resources/resource.png" alt="ressource" class="main" />';
 		echo '<h2>Envoi de ressources</h2>';
@@ -8,14 +8,37 @@ echo '<div class="component new-message">';
 	echo '<div class="fix-body">';
 		echo '<div class="body">';
 			echo '<form action="' . APP_ROOT . 'action/a-giveresource/baseid-' . $ob_compPlat->rPlace . '" method="post">';
+				# base
 				echo '<p><label for="send-resources-target">Base destinataire</label></p>';
 				echo '<input class="autocomplete-hidden" type="hidden" name="otherbaseid" />';
 				echo '<p class="input input-text"><input type="text" id="send-resources-target" class="autocomplete-orbitalbase" name="name" autocomplete="off" /></p>';
 
-				echo '<p><label for="send-resources-quantity">Nombre de ressources</label></p>';
-				echo '<p class="input input-text"><input type="text" id="send-resources-quantity" name="quantity" /></p>';
+				# form de vente
+				echo '<div class="sell-form" data-shipcom-size="' . CommercialShipping::WEDGE . '" data-resource-rate="1" data-max-quantity="' . $ob_compPlat->resourcesStorage . '" data-rate="0" data-min-price="0">';
+					echo '<div class="label-box">';
+						echo '<span class="label">Ressources</span>';
+						echo '<span class="value">' . Format::numberFormat($ob_compPlat->resourcesStorage) . '</span>';
+						echo '<img class="icon-color" alt="ressources" src="' . MEDIA . 'resources/resource.png">';
+					echo '</div>';
 
-				echo '<p class="button"><button type="submit">Envoyer</button></p>';
+					echo '<div class="label-box sf-quantity">';
+						echo '<label for="send-resources-quantity" class="label">Quantité</label>';
+						echo '<input id="send-resources-quantity" class="value" type="text" name="quantity" autocomplete="off" />';
+						echo '<img class="icon-color" alt="ressources" src="' . MEDIA . 'resources/resource.png">';
+					echo '</div>';
+
+					echo '<hr />';
+
+					echo '<div class="label-box sf-comship">';
+						echo '<span class="label">Vaisseaux</span>';
+						echo '<span class="value"></span>';
+						echo '<img class="icon-color" alt="vaisseaux transports" src="' . MEDIA . 'resources/transport.png">';
+					echo '</div>';
+
+					echo '<hr />';
+
+					echo '<p><input type="submit" value="Vendre" /></p>';
+				echo '</div>';
 			echo '</form>';
 		echo '</div>';
 	echo '</div>';
