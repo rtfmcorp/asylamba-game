1d59412bec395afc276c108086d96f2247231fe6
acknowledge <jack_ett8@hotmail.com>
2015-01-02 19:36:21 +0100

    corrections orthographiques

--- system/views/components/bases/ob/leavebase.php
+++ system/views/components/bases/ob/leavebase.php
@@ -21,8 +21,8 @@ echo '<div class="component generator">';
 		echo '<div class="body">';
 			echo '<h4>Abandonner cette planète</h4>';
 
-			echo '<p>Pour abandonner une planète, aucun de vos commandants ne doivent être en mission.</p>';
-			echo '<p>Une planète abandonnée peut être conquise par n\'importe qui. Les commandants en orbite autour de celle-ci continuent à la défendre. Les vaisseaux dans le hangar sont conservés. Cepandant, les routes commerciales ainsi que les offres de ventes sont supprimées. De plus toutes les constuctions programmées sont annulées.</p>';
+			echo '<p>Pour abandonner une planète, aucun de vos commandants ne doit être en mission.</p>';
+			echo '<p>Une planète abandonnée peut être conquise par n\'importe qui. Les commandants en orbite autour de celle-ci continuent à la défendre. Les vaisseaux dans le hangar sont conservés. Cependant, les routes commerciales ainsi que les offres de ventes sont supprimées. De plus, toutes les constructions programmées sont annulées.</p>';
 
 			echo '<hr>';
 
