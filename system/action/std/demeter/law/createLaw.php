8baf2c1058ac572a6b9a2bbf34acac438867d063
Estiocle <noe.zufferey@gmail.com>
2015-02-10 13:57:46 +0100

    suppresion du chemin APOLLON dans index.default

--- system/action/std/demeter/law/createLaw.php
+++ system/action/std/demeter/law/createLaw.php
@@ -4,7 +4,7 @@ include_once ZEUS;
 include_once GAIA;
 
 #type
-#duaration pour les lois à durée en seconde
+#duration pour les lois à durée en seconde
 #taxes taux de taxe
 #rColor autre faction concernée
 #rSector secteur concernée
