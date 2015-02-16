3a86c5d925bb07d7085d5922510c5aa2d23b5d41
acknowledge <jack_ett8@hotmail.com>
2015-02-16 08:56:24 +0100

    limitation de l'investissement dans l'uni

--- system/action/std/zeus/player/updateUniInvest.php
+++ system/action/std/zeus/player/updateUniInvest.php
@@ -8,23 +8,27 @@ $credit = Utils::getHTTPData('credit');
 
 
 if ($credit !== FALSE) { 
-	$S_PAM1 = ASM::$pam->getCurrentSession();
-	ASM::$pam->newSession();
-	ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
-	ASM::$pam->get()->iUniversity = $credit;
+	if ($credit <= 500000) {
+		$S_PAM1 = ASM::$pam->getCurrentSession();
+		ASM::$pam->newSession();
+		ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
+		ASM::$pam->get()->iUniversity = $credit;
 
-	# tutorial
-	if (CTR::$data->get('playerInfo')->get('stepDone') == FALSE) {
-		switch (CTR::$data->get('playerInfo')->get('stepTutorial')) {
-			case TutorialResource::MODIFY_UNI_INVEST:
-				TutorialHelper::setStepDone();
-				break;
+		# tutorial
+		if (CTR::$data->get('playerInfo')->get('stepDone') == FALSE) {
+			switch (CTR::$data->get('playerInfo')->get('stepTutorial')) {
+				case TutorialResource::MODIFY_UNI_INVEST:
+					TutorialHelper::setStepDone();
+					break;
+			}
 		}
+
+		CTR::$alert->add('L\'investissement dans l\'université a été modifié', ALERT_STD_SUCCESS);
+
+		ASM::$pam->changeSession($S_PAM1);
+	} else {
+		CTR::$alert->add('La limite maximale d\'investissement dans l\'Université est de 500\'000 crédits.', ALERT_STD_ERROR);
 	}
-
-	CTR::$alert->add('L\'investissement dans l\'université a été modifié', ALERT_STD_SUCCESS);
-
-	ASM::$pam->changeSession($S_PAM1);
 } else {
 	CTR::$alert->add('pas assez d\'informations pour modifier cet investissement', ALERT_STD_FILLFORM);
 }
