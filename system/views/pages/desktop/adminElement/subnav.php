0f59efa84d828516c1ca34d50e31dec5bdfa7be2
abdelaz3r <gil.clavien@gmail.com>
2015-03-06 13:31:38 +0100

    correction : page d'admin

--- system/views/pages/desktop/adminElement/subnav.php
+++ system/views/pages/desktop/adminElement/subnav.php
@@ -2,17 +2,7 @@
 echo '<div id="subnav">';
 	echo '<button class="move-side-bar top" data-dir="up"> </button>';
 	echo '<div class="overflow">';
-		$active = (!CTR::$get->exist('view') OR CTR::$get->get('view') == 'bugtracker') ? 'active' : '';
-		echo '<a href="' . APP_ROOT . 'admin/view-bugtracker" class="item ' . $active . '">';
-			echo '<span class="picto">';
-				echo '<img src="' . MEDIA . 'admin/bugtracker.png" alt="" />';
-			echo '</span>';
-			echo '<span class="content skin-1">';
-				echo '<span>Bugtracker';
-			echo '</span>';
-		echo '</a>';
-		
-		$active = (CTR::$get->get('view') == 'message') ? 'active' : '';
+		$active = (!CTR::$get->exist('view') OR CTR::$get->get('view') == 'message') ? 'active' : '';
 		echo '<a href="' . APP_ROOT . 'admin/view-message" class="item ' . $active . '">';
 			echo '<span class="picto">';
 				echo '<img src="' . MEDIA . 'admin/main.png" alt="" />';
