f721a8acf9dd98ae4a3454815683243041295a54
acknowledge <jack_ett8@hotmail.com>
2015-03-20 00:30:28 +0100

    on écrit 'statut' et non 'status' qui est en anglais

--- system/views/components/faction/data/diplomacy/main.php
+++ system/views/components/faction/data/diplomacy/main.php
@@ -12,7 +12,7 @@ $statements = [
 
 echo '<div class="component player rank">';
 	echo '<div class="head skin-2">';
-		echo '<h2>Status diplomatique</h2>';
+		echo '<h2>Statut diplomatique</h2>';
 	echo '</div>';
 	echo '<div class="fix-body">';
 		echo '<div class="body">';
