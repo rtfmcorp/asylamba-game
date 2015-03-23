70d0707ace362042efebe5d55121255807b4fe5a
acknowledge <jack_ett8@hotmail.com>
2015-03-23 22:27:34 +0100

    corrections orthographiques dans les descriptions de pactes

--- system/views/components/faction/data/diplomacy/about.php
+++ system/views/components/faction/data/diplomacy/about.php
@@ -20,12 +20,12 @@ echo '<div class="component player rank">';
 			echo '<p>Lors de la déclaration de guerre, toutes les routes commerciales entre les deux factions belligérantes sont coupées. Durant toute la durée de la guerre, aucun lien commercial ne sera possible.</p>';
 
 			echo '<h4>Allié</h4>';
-			echo '<p>Un alliance totale est un fort lien entre deux factions. Ce traité n\'est pas contraignant mais les attaques le violant sont notifiées les registres de guerre de la faction. </p>';
+			echo '<p>Une alliance totale est un fort lien entre deux factions. Ce traité n\'est pas contraignant mais les attaques le violant sont notifiées dans les registres de guerre de la faction.</p>';
 			echo '<p>Un seul pacte de ce type peut être rédigé par faction.</p>';
 
 			echo '<h4>Pacte de non-agression</h4>';
-			echo '<p>Un pacte de non-agression n\'est pas contraignant mais garanti que les membres des deux factions ne s\'attaquent pas entre eux.</p>';
-			echo '<p>Les attaques violant ce pacte sont notifiées les registres de guerre de la faction. Ce type de pacte est limité à 2 par faction.</p>';
+			echo '<p>Un pacte de non-agression n\'est pas contraignant mais garantit que les membres des deux factions ne s\'attaquent pas entre eux. Les attaques violant ce pacte sont notifiées dans les registres de guerre de la faction. </p>';
+			echo '<p>Ce type de pacte est limité à deux par faction.</p>';
 
 			echo '<h4>Neutre</h4>';
 			echo '<p>Une faction n\'a ni affinités ni problèmes avec une autre faction neutre. De petits problèmes territoriaux peuvent exister sans remettre en cause ce statut.</p>';
