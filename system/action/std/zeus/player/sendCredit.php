e1ddc94a9cbad8dbe1913ae1be31cfb23f1d0d72
acknowledge <jack_ett8@hotmail.com>
2015-04-21 19:33:48 +0200

    envoi de crédit : alerte quand le texte est trop long

--- system/action/std/zeus/player/sendCredit.php
+++ system/action/std/zeus/player/sendCredit.php
@@ -15,64 +15,68 @@ $p = new Parser();
 $name = $p->protect($name);
 $text = $p->parse($text);
 
-if ($name !== FALSE AND $quantity !== FALSE AND strlen($text) < 500) {
-	$credit = intval($quantity);
+if ($name !== FALSE AND $quantity !== FALSE) {
+	if (strlen($text) < 500) {
+		$credit = intval($quantity);
 
-	if ($credit > 0) {
+		if ($credit > 0) {
 
-		$S_PAM1 = ASM::$pam->getCurrentSession();
-		ASM::$pam->newSession(ASM_UMODE);
-		ASM::$pam->load(array('name' => $name));
-		ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
+			$S_PAM1 = ASM::$pam->getCurrentSession();
+			ASM::$pam->newSession(ASM_UMODE);
+			ASM::$pam->load(array('name' => $name));
+			ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
 
-		if (ASM::$pam->size() == 2) {
-			$receiver = ASM::$pam->get();
-			$sender = ASM::$pam->get(1);
+			if (ASM::$pam->size() == 2) {
+				$receiver = ASM::$pam->get();
+				$sender = ASM::$pam->get(1);
 
-			if ($sender->credit >= $credit) {
-				$sender->decreaseCredit($credit);
-				$receiver->increaseCredit($credit);
+				if ($sender->credit >= $credit) {
+					$sender->decreaseCredit($credit);
+					$receiver->increaseCredit($credit);
 
-				# create the transaction
-				$ct = new CreditTransaction();
-				$ct->rSender = CTR::$data->get('playerId');
-				$ct->type = CreditTransaction::TYP_PLAYER;
-				$ct->rReceiver = $receiver->id;
-				$ct->amount = $credit;
-				$ct->dTransaction = Utils::now();
-				$ct->comment = $text;
-				ASM::$crt->add($ct);
+					# create the transaction
+					$ct = new CreditTransaction();
+					$ct->rSender = CTR::$data->get('playerId');
+					$ct->type = CreditTransaction::TYP_PLAYER;
+					$ct->rReceiver = $receiver->id;
+					$ct->amount = $credit;
+					$ct->dTransaction = Utils::now();
+					$ct->comment = $text;
+					ASM::$crt->add($ct);
+							
+					$n = new Notification();
+					$n->setRPlayer($receiver->id);
+					$n->setTitle('Réception de crédits');
+					$n->addBeg();
+					$n->addLnk('embassy/player-' . CTR::$data->get('playerId'), CTR::$data->get('playerInfo')->get('name'));
+					$n->addTxt(' vous a envoyé des crédits');
+					if ($text !== '') {
+						$n->addTxt(' avec le message suivant : ')->addBrk()->addTxt('"' . $text . '"');
+					} else {
+						$n->addTxt('.');
+					}
+					$n->addBoxResource('credit', Format::numberFormat($credit), 'crédits reçus');
+					$n->addEnd();
+					ASM::$ntm->add($n);
+
+					CTR::$alert->add('Crédits envoyés', ALERT_STD_SUCCESS);
 						
-				$n = new Notification();
-				$n->setRPlayer($receiver->id);
-				$n->setTitle('Réception de crédits');
-				$n->addBeg();
-				$n->addLnk('embassy/player-' . CTR::$data->get('playerId'), CTR::$data->get('playerInfo')->get('name'));
-				$n->addTxt(' vous a envoyé des crédits');
-				if ($text !== '') {
-					$n->addTxt(' avec le message suivant : ')->addBrk()->addTxt('"' . $text . '"');
 				} else {
-					$n->addTxt('.');
+					CTR::$alert->add('envoi de crédits impossible - vous ne pouvez pas envoyer plus que ce que vous possédez', ALERT_STD_ERROR);
 				}
-				$n->addBoxResource('credit', Format::numberFormat($credit), 'crédits reçus');
-				$n->addEnd();
-				ASM::$ntm->add($n);
-
-				CTR::$alert->add('Crédits envoyés', ALERT_STD_SUCCESS);
-					
 			} else {
-				CTR::$alert->add('envoi de crédits impossible - vous ne pouvez pas envoyer plus que ce que vous possédez', ALERT_STD_ERROR);
+				if (ASM::$pam->size() == 1) {
+					CTR::$alert->add('envoi de crédits impossible - aucun intérêt d\'envoyer des crédits à vous-même !?', ALERT_STD_ERROR);
+				} else {
+					CTR::$alert->add('envoi de crédits impossible - erreur dans les joueurs', ALERT_STD_ERROR);
+				}
 			}
+			ASM::$pam->changeSession($S_PAM1);
 		} else {
-			if (ASM::$pam->size() == 1) {
-				CTR::$alert->add('envoi de crédits impossible - aucun intérêt d\'envoyer des crédits à vous-même !?', ALERT_STD_ERROR);
-			} else {
-				CTR::$alert->add('envoi de crédits impossible - erreur dans les joueurs', ALERT_STD_ERROR);
-			}
+			CTR::$alert->add('envoi de crédits impossible - il faut envoyer un nombre positif', ALERT_STD_ERROR);
 		}
-		ASM::$pam->changeSession($S_PAM1);
 	} else {
-		CTR::$alert->add('envoi de crédits impossible - il faut envoyer un nombre positif', ALERT_STD_ERROR);
+		CTR::$alert->add('le texte ne doit pas dépasser les 500 caractères', ALERT_STD_FILLFORM);
 	}
 } else {
 	CTR::$alert->add('pas assez d\'informations pour envoyer des crédits', ALERT_STD_FILLFORM);
