3b93a184e699ec529eff8f658bee8201f898f0bf
abdelaz3r <gil.clavien@gmail.com>
2015-07-23 09:24:01 +0200

    add analitycs datas

--- system/views/templates/desktop/open.php
+++ system/views/templates/desktop/open.php
@@ -38,14 +38,16 @@ echo '</head>';
 if (ANALYTICS) {
 ?>
 <script>
+	ga('set', '&uid', <?php echo CTR::$data->get('playerId'); ?>);
+
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-42636532-5', 'auto');
+	ga('create', 'UA-42636532-6', 'auto');
 	ga('send', 'pageview');
-	ga('set', '&uid', <?php echo CTR::$data->get('playerId'); ?>);
+	ga(‘set’, ‘&uid’, <?php echo CTR::$data->get('playerId'); ?>);
 </script>
 <?php
 }
