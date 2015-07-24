fb0725ce98e15500c878fe7653f5c5783110054f
abdelaz3r <gil.clavien@gmail.com>
2015-07-24 13:56:18 +0200

    recalibrage des dates

--- system/classes/lib/Chronos.class.php
+++ system/classes/lib/Chronos.class.php
@@ -15,7 +15,7 @@ class Chronos {
 	const LN_SEC	= 'seconde';
 
 	const REAL_TIME = SERVER_START_TIME;
-	const SEG_SHIFT = 500;
+	const SEG_SHIFT = 260;
 	
 	/*
 	 * retourne le temps restant avant la prochaine relève
