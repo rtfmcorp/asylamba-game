017faffe04c8334a1a4c54cbd3253b006a6b8c88
acknowledge <jack_ett8@hotmail.com>
2015-10-07 18:25:44 +0200

    bugfix : on peut annuler une route en standby a cause d'une guerre

--- system/action/std/athena/route/delete.php
+++ system/action/std/athena/route/delete.php
@@ -15,7 +15,7 @@ $route = Utils::getHTTPData('route');
 if ($base !== FALSE AND $route !== FALSE AND in_array($base, $verif)) {
 	$S_CRM1 = ASM::$crm->getCurrentSession();
 	ASM::$crm->newSession(ASM_UMODE);
-	ASM::$crm->load(array('id'=>$route, 'statement' => CRM_ACTIVE));
+	ASM::$crm->load(array('id' => $route, 'statement' => [CRM_ACTIVE, CRM_STANDBY]));
 	if (ASM::$crm->get() && ASM::$crm->size() == 1) {
 		$cr = ASM::$crm->get();
 		if ($cr->playerId1 == CTR::$data->get('playerId') || $cr->playerId2 == CTR::$data->get('playerId')) {
