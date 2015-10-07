816a391ff7ac9b3026f59a6e0555ff8b388d325e
acknowledge <jack_ett8@hotmail.com>
2015-10-07 14:53:23 +0200

    essai d'ajout de publicité dans la page paramètres

--- system/views/components/default.php
+++ system/views/components/default.php
@@ -1,6 +1,28 @@
 <div class="component">
 	<div class="head"></div>
 	<div class="fix-body">
-		<div class="body"></div>
+		<div class="body">
+
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- BlocIG_txt -->
+			<ins class="adsbygoogle"
+			     style="display:inline-block;width:300px;height:250px"
+			     data-ad-client="ca-pub-8216044585498923"
+			     data-ad-slot="3838918699"></ins>
+			<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+
+
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- BlocIG_imgAndTxt -->
+			<ins class="adsbygoogle"
+			     style="display:inline-block;width:300px;height:250px"
+			     data-ad-client="ca-pub-8216044585498923"
+			     data-ad-slot="4397321898"></ins>
+			<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
 </div>
