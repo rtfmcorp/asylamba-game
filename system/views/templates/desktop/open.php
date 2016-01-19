82597b8fdf58f5f65af2ca9dfe8c003aef1bd06d
acknowledge <jack_ett8@hotmail.com>
2016-01-19 10:15:14 +0100

    update analytics

--- system/views/templates/desktop/open.php
+++ system/views/templates/desktop/open.php
@@ -43,7 +43,7 @@ if (ANALYTICS) {
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-42636532-7', 'auto');
+	ga('create', 'UA-42636532-8', 'auto');
 	ga('send', 'pageview');
 </script>
 <?php
