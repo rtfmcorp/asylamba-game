c9b2d2e2ddaae3b56bd84b456824b6060229d855
acknowledge <jack_ett8@hotmail.com>
2016-07-03 19:16:03 +0200

    config pour la partie s11

--- system/config/app.config.install.php
+++ system/config/app.config.install.php
@@ -1,3 +1,11 @@
 <?php
 # liste les IDs de factions disponibles
-$AVAILABLE_FACTIONS	= [1, 2, 6, 7, 11];
+
+# s7/s11 - révolte seldarine
+$AVAILABLE_FACTIONS = [1, 4, 8, 9];
+# s8 - révolution cardanienne
+#$AVAILABLE_FACTIONS = [4, 8, 10, 11, 12];
+# s9 - empire contre-attaque
+#$AVAILABLE_FACTIONS = [1, 8, 11];
+# s10 - chute des archontes
+#$AVAILABLE_FACTIONS	= [1, 2, 6, 7, 11];
