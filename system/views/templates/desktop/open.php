a011facb32da69fd9979c4b9d0610853f037a801
acknowledge <jack_ett8@hotmail.com>
2016-09-25 22:04:38 +0200

    ad dnew ganalytics for s12

--- system/views/templates/desktop/open.php
+++ system/views/templates/desktop/open.php
@@ -43,7 +43,7 @@ if (ANALYTICS) {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-42636532-10', 'auto');
+  ga('create', 'UA-42636532-11', 'auto');
   ga('send', 'pageview');
 
 </script>
