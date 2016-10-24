b2872eaba062848332f45dba110b817dd787df87
Axel Venet <kern046@gmail.com>
2016-10-25 00:26:15 +0200

    Update the index entrypoint

--- index.default.php
+++ index.default.php
@@ -1,7 +1,13 @@
 <?php
+
+require_once('vendor/autoload.php');
+
+use Asylamba\Classes\Worker\CTR;
+
 # définition des ROOT
 define('PUBLICR',		'http://localhost/[ your path here ]/public/');
-define('SYSTEMR',		'system/');
+define('SYSTEMR',		__DIR__ . '/system/');
+define('CLASSES',               SYSTEMR . 'Classes/');
 
 # définition des ROOT
 define('CSS', 			PUBLICR . 'css/');
@@ -9,8 +15,7 @@ define('JS', 		 	PUBLICR . 'js/');
 define('MEDIA', 		PUBLICR . 'media/');
 define('LOG', 			PUBLICR . 'log/');
 
-define('MODULES', 		SYSTEMR . 'modules/');
-define('CLASSES',		SYSTEMR . 'classes/');
+define('MODULES', 		SYSTEMR . 'Modules/');
 define('LIB', 			CLASSES . 'lib/');
 define('CONFIG', 		SYSTEMR . 'config/');
 define('EVENT', 		SYSTEMR . 'event/');
@@ -29,28 +34,24 @@ define('TEMPLATE', 		SYSTEMR . 'views/templates/');
 define('PAGES', 		SYSTEMR . 'views/pages/');
 define('COMPONENT', 	SYSTEMR . 'views/components/');
 
-# définition des ROOT des MODULES
-define('ARES', 			MODULES . 'ares/main.php');
-define('HERMES', 		MODULES . 'hermes/main.php');
-define('GAIA', 			MODULES . 'gaia/main.php');
-define('ZEUS', 			MODULES . 'zeus/main.php');
-define('ATHENA', 		MODULES . 'athena/main.php');
-define('PROMETHEE', 	MODULES . 'promethee/main.php');
-define('ARTEMIS', 		MODULES . 'artemis/main.php');
-define('APOLLON', 		MODULES . 'apollon/main.php');
-define('DEMETER', 		MODULES . 'demeter/main.php');
-define('ATLAS',			MODULES . 'atlas/main.php');
-
 # inclusion des fichiers de configurations
 include CONFIG . 'app.config.local.php';
 include CONFIG . 'app.config.global.php';
 
-# inclusion des classes
-include CLASSES . 'loader.php';
+# définition des ROOT des MODULES
+include_once MODULES . 'Ares/main.php';
+include_once MODULES . 'Hermes/main.php';
+include_once MODULES . 'Gaia/main.php';
+include_once MODULES . 'Zeus/main.php';
+include_once MODULES . 'Athena/main.php';
+include_once MODULES . 'Promethee/main.php';
+include_once MODULES . 'Artemis/main.php';
+//include_once MODULES . 'Apollon/main.php';
+include_once MODULES . 'Demeter/main.php';
+include_once MODULES . 'Atlas/main.php';
 
 # Action du controller
 CTR::init();
 CTR::checkPermission();
 CTR::getInclude();
 CTR::save();
-?>
