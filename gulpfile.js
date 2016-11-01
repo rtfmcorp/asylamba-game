acb3f5962fa826f98d4610b9698dfdf769c6cfc2
acknowledge <jack_ett8@hotmail.com>
2016-11-01 16:51:35 +0100

    add gulp-less to dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var less = require('gulp-less');
 
-gulp.task('default', function() {
+gulp.task('default', ['less'], function() {
 	return gutil.log('Gulp is running!')
+});
+
+
+gulp.task('less', function () {
+	return gulp.src('./public/css/less/*.less')
+		//.pipe(less({
+		//	paths: [ path.join(__dirname, 'less', 'includes') ]
+		//}))
+		.pipe(less())
+		.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('lessexemple', function(){
+  gulp.src('./normal.less')
+  .pipe(less())
+  .pipe(gulp.dest('build'));
 });
