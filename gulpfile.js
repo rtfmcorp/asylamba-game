3d77feeda0e39fd790dccf2a556de5a490de6915
acknowledge <jack_ett8@hotmail.com>
2016-11-01 18:29:39 +0100

    less file can be compiles with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var less = require('gulp-less');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 gulp.task('default', ['less'], function() {
 	return gutil.log('Gulp is running!')
 });
 
-
-gulp.task('less', function () {
-	return gulp.src('./public/css/less/*.less')
-		//.pipe(less({
-		//	paths: [ path.join(__dirname, 'less', 'includes') ]
-		//}))
+gulp.task('less', function(){
+	// specific faction files
+	for (var i = 1; i < 13; i++) {
+		gulp.src(['./public/css/less/main.desktop.v3.less'])
+			.pipe(replace('@color-type: 1;', '@color-type: '.concat(i, ';')))
+			.pipe(less())
+			.pipe(rename('main.desktop.v3.color' + i + '.css'))
+			.pipe(gulp.dest('./public/css/'));
+	}
+	// general file
+	gulp.src(['./public/css/less/main.desktop.v3.less'])
 		.pipe(less())
-		.pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('lessexemple', function(){
-  gulp.src('./normal.less')
-  .pipe(less())
-  .pipe(gulp.dest('build'));
+		.pipe(gulp.dest('./public/css/'));
 });
