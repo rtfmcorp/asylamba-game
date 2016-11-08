9e247d38d2032b82bbf344c138736023c8a37551
Axel VENET <axel.venet@fr.airtag.com>
2016-11-08 15:39:52 +0100

    Update the test suite to test the modules main classes

---
+++ tests/Modules/Demeter/DemeterModuleTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Asylamba\Modules\Demeter;
+
+use Asylamba\Modules\Demeter\DemeterModule;
+use Asylamba\Classes\Worker\Application;
+use Asylamba\Classes\Worker\Container;
+
+class DemeterModuleTest extends \PHPUnit_Framework_TestCase
+{
+	/** @var DemeterModule **/
+	protected $module;
+	
+	public function setUp()
+	{
+		$this->module = new DemeterModule($this->getApplicationMock());
+	}
+	
+	public function testGetName()
+	{
+		$this->assertEquals('Demeter', $this->module->getName());
+	}
+	
+	public function getApplicationMock()
+	{
+		$applicationMock = $this
+			->getMockBuilder(Application::class)
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$applicationMock
+			->expects($this->any())
+			->method('getContainer')
+			->willReturnCallback([$this, 'getContainerMock'])
+		;
+		return $applicationMock;
+	}
+	
+	public function getContainerMock()
+	{
+		$containerMock = $this
+			->getMockBuilder(Container::class)
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$containerMock
+			->expects($this->any())
+			->method('getParameter')
+			->willReturn(realpath('.'))
+		;
+		return $containerMock;
+	}
+}
