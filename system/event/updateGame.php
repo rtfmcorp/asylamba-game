d72cfc0744f3f9335edfa81018045cbe2fee5dd1
Axel Venet <kern046@gmail.com>
2016-12-16 11:27:02 +0100

    Update event

--- system/event/updateGame.php
+++ system/event/updateGame.php
@@ -2,18 +2,19 @@
 # met à jour tout ce qui concerne un joueur
 
 use Asylamba\Classes\Library\Utils;
-use Asylamba\Classes\Worker\CTR;
-use Asylamba\Classes\Worker\ASM;
+
+$session = $this->getContainer()->get('app.session');
+$playerManager = $this->getContainer()->get('zeus.player_manager');
 
 # mise à jour chaque heure
-if (Utils::interval(CTR::$data->get('lastUpdate')->get('game'), Utils::now(), 'h') > 0) {
+if (Utils::interval($session->get('lastUpdate')->get('game'), Utils::now(), 'h') > 0) {
 	# update de l'heure dans le contrôleur
-	CTR::$data->get('lastUpdate')->add('game', Utils::now());
+	$session->get('lastUpdate')->add('game', Utils::now());
 
 	# mise à jour de tout
-	$S_PAM1 = ASM::$pam->getCurrentSession();
-	ASM::$pam->newSession(ASM_UMODE);
-	ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
+	$S_PAM1 = $playerManager->getCurrentSession();
+	$playerManager->newSession(ASM_UMODE);
+	$playerManager->load(array('id' => $session->get('playerId')));
 	# --> uActionPoint et uCredit
 		# --> instancie toutes les bases orbitales
 			# --> uBuildingQueue
@@ -25,31 +26,31 @@ if (Utils::interval(CTR::$data->get('lastUpdate')->get('game'), Utils::now(), 'h
 			# --> uTravel
 			# --> uUxperienceInSchool
 		# --> instancie les recherches et fait l'update
-	ASM::$pam->changeSession($S_PAM1);
+	$playerManager->changeSession($S_PAM1);
 }
 
 # mise à jour des sessions de temps en temps
-/*if (Utils::interval(CTR::$data->get('lastUpdate')->get('game'), Utils::now(), 's') > 3) {
+/*if (Utils::interval($session->get('lastUpdate')->get('game'), Utils::now(), 's') > 3) {
 	# update de l'heure dans le contrôleur
-	CTR::$data->get('lastUpdate')->add('game', Utils::now());
+	$session->get('lastUpdate')->add('game', Utils::now());
 
 	# on récupère l'id
-	$id = CTR::$data->get('playerId');
+	$id = $session->get('playerId');
 
 	# on vide les sessions
-	CTR::$data->clear();
+	$session->clear();
 
 	# on charge le joueur
-	$S_PAM_UG = ASM::$pam->getCurrentSession();
-	ASM::$pam->newSession();
-	ASM::$pam->load(array('id' => $id));
+	$S_PAM_UG = $playerManager->getCurrentSession();
+	$playerManager->newSession();
+	$playerManager->load(array('id' => $id));
 
-	$player = ASM::$pam->get();
+	$player = $playerManager->get();
 	var_dump($player);
 	exit();
 	include_once CONNECTION . 'create-session.php';
 
-	ASM::$pam->changeSession($S_PAM_UG);
+	$playerManager->changeSession($S_PAM_UG);
 
 	# on log pour faire joli
 	$path = 'public/log/stats/tmp.log';
