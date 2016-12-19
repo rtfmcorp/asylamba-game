68f403e79bdb25ca221b94ab26423220fb2d5c67
acknowledge <jack_ett8@hotmail.com>
2016-12-19 21:45:22 +0100

    Close #47 : optimize images with a Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
+'use strict';
+
+// Include Gulp & tools we'll use
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['less'], function() {
+
+// -------------------- GULP TASKS --------------------
+
+gulp.task('default', ['less'], () => {
 	return gutil.log('Gulp is running!')
 });
 
-gulp.task('less', function(){
+// Build CSS with LESS sources
+gulp.task('less', () => {
 	// specific faction files
 	for (var i = 1; i < 13; i++) {
 		gulp.src(['./public/css/less/main.desktop.v3.less'])
@@ -21,4 +29,14 @@ gulp.task('less', function(){
 	gulp.src(['./public/css/less/main.desktop.v3.less'])
 		.pipe(less())
 		.pipe(gulp.dest('./public/css/'));
+});
+
+// Optimize images
+gulp.task('images', () => {
+	gulp.src('public/css/src/desktop-src/**/*')
+		.pipe(imagemin({ progressive: true}))
+		.pipe(gulp.dest('public/css/src/desktop'))
+	gulp.src('public/media-src/**/*')
+		.pipe(imagemin({ progressive: true }))
+		.pipe(gulp.dest('public/media'))
 });
