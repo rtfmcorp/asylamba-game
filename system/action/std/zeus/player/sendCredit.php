5040e991963aecc7b4609d4a9eb55066252111b0
Axel Venet <kern046@gmail.com>
2016-12-28 20:48:04 +0100

    Fix credit sending to player

--- system/action/std/zeus/player/sendCredit.php
+++ system/action/std/zeus/player/sendCredit.php
@@ -6,9 +6,11 @@ use Asylamba\Classes\Library\Utils;
 use Asylamba\Classes\Library\Format;
 use Asylamba\Classes\Library\Parser;
 use Asylamba\Classes\Library\DataAnalysis;
-use Asylamba\Classes\Database\Database;
 use Asylamba\Modules\Zeus\Model\CreditTransaction;
 use Asylamba\Modules\Hermes\Model\Notification;
+use Asylamba\Classes\Library\Http\Response;
+use Asylamba\Classes\Exception\ErrorException;
+use Asylamba\Classes\Exception\FormException;
 
 # give credit action
 
@@ -16,14 +18,20 @@ use Asylamba\Modules\Hermes\Model\Notification;
 # int quantity 		quantity of credit to send
 # [string text] 	facultative text
 
-$name = Utils::getHTTPData('name');
-$quantity = Utils::getHTTPData('quantity');
-$text = Utils::getHTTPData('text');
+$request = $this->getContainer()->get('app.request');
+$session = $this->getContainer()->get('app.session');
+$playerManager = $this->getContainer()->get('zeus.player_manager');
+$creditTransactionManager = $this->getContainer()->get('zeus.credit_transaction_manager');
+$notificationManager = $this->getContainer()->get('hermes.notification_manager');
+$parser = $this->getContainer()->get('parser');
+
+$name = $request->request->get('name');
+$quantity = $request->request->get('quantity');
+$text = $request->request->get('text');
 
 // input protection
-$p = new Parser();
-$name = $p->protect($name);
-$text = $p->parse($text);
+$name = $parser->protect($name);
+$text = $parser->parse($text);
 
 if ($name !== FALSE AND $quantity !== FALSE) {
 	if (strlen($text) < 500) {
@@ -31,34 +39,34 @@ if ($name !== FALSE AND $quantity !== FALSE) {
 
 		if ($credit > 0) {
 
-			$S_PAM1 = ASM::$pam->getCurrentSession();
-			ASM::$pam->newSession(ASM_UMODE);
-			ASM::$pam->load(array('name' => $name));
-			ASM::$pam->load(array('id' => CTR::$data->get('playerId')));
+			$S_PAM1 = $playerManager->getCurrentSession();
+			$playerManager->newSession(ASM_UMODE);
+			$playerManager->load(array('name' => $name));
+			$playerManager->load(array('id' => $session->get('playerId')));
 
-			if (ASM::$pam->size() == 2) {
-				$receiver = ASM::$pam->get();
-				$sender = ASM::$pam->get(1);
+			if ($playerManager->size() == 2) {
+				$receiver = $playerManager->get();
+				$sender = $playerManager->get(1);
 
 				if ($sender->credit >= $credit) {
-					$sender->decreaseCredit($credit);
-					$receiver->increaseCredit($credit);
+					$playerManager->decreaseCredit($sender, $credit);
+					$playerManager->increaseCredit($receiver, $credit);
 
 					# create the transaction
 					$ct = new CreditTransaction();
-					$ct->rSender = CTR::$data->get('playerId');
+					$ct->rSender = $session->get('playerId');
 					$ct->type = CreditTransaction::TYP_PLAYER;
 					$ct->rReceiver = $receiver->id;
 					$ct->amount = $credit;
 					$ct->dTransaction = Utils::now();
 					$ct->comment = $text;
-					ASM::$crt->add($ct);
+					$creditTransactionManager->add($ct);
 							
 					$n = new Notification();
 					$n->setRPlayer($receiver->id);
 					$n->setTitle('Réception de crédits');
 					$n->addBeg();
-					$n->addLnk('embassy/player-' . CTR::$data->get('playerId'), CTR::$data->get('playerInfo')->get('name'));
+					$n->addLnk('embassy/player-' . $session->get('playerId'), $session->get('playerInfo')->get('name'));
 					$n->addTxt(' vous a envoyé des crédits');
 					if ($text !== '') {
 						$n->addTxt(' avec le message suivant : ')->addBrk()->addTxt('"' . $text . '"');
@@ -67,36 +75,35 @@ if ($name !== FALSE AND $quantity !== FALSE) {
 					}
 					$n->addBoxResource('credit', Format::numberFormat($credit), ($credit == 1 ? 'crédit reçu' : 'crédits reçus'));
 					$n->addEnd();
-					ASM::$ntm->add($n);
+					$notificationManager->add($n);
 
 					if (DATA_ANALYSIS) {
-						$db = Database::getInstance();
-						$qr = $db->prepare('INSERT INTO 
+						$qr = $database->prepare('INSERT INTO 
 							DA_CommercialRelation(`from`, `to`, type, weight, dAction)
 							VALUES(?, ?, ?, ?, ?)'
 						);
 						$qr->execute([$ct->rSender, $ct->rReceiver, 5, DataAnalysis::creditToStdUnit($ct->amount), Utils::now()]);
 					}
 
-					CTR::$alert->add('Crédits envoyés', ALERT_STD_SUCCESS);
+					$response->flashbag->add('Crédits envoyés', Response::FLASHBAG_SUCCESS);
 						
 				} else {
-					CTR::$alert->add('envoi de crédits impossible - vous ne pouvez pas envoyer plus que ce que vous possédez', ALERT_STD_ERROR);
+					throw new ErrorException('envoi de crédits impossible - vous ne pouvez pas envoyer plus que ce que vous possédez');
 				}
 			} else {
-				if (ASM::$pam->size() == 1) {
-					CTR::$alert->add('envoi de crédits impossible - aucun intérêt d\'envoyer des crédits à vous-même !?', ALERT_STD_ERROR);
+				if ($playerManager->size() == 1) {
+					throw new ErrorException('envoi de crédits impossible - aucun intérêt d\'envoyer des crédits à vous-même !?');
 				} else {
-					CTR::$alert->add('envoi de crédits impossible - erreur dans les joueurs', ALERT_STD_ERROR);
+					throw new ErrorException('envoi de crédits impossible - erreur dans les joueurs');
 				}
 			}
-			ASM::$pam->changeSession($S_PAM1);
+			$playerManager->changeSession($S_PAM1);
 		} else {
-			CTR::$alert->add('envoi de crédits impossible - il faut envoyer un nombre entier positif', ALERT_STD_ERROR);
+			throw new ErrorException('envoi de crédits impossible - il faut envoyer un nombre entier positif');
 		}
 	} else {
-		CTR::$alert->add('le texte ne doit pas dépasser les 500 caractères', ALERT_STD_FILLFORM);
+		throw new FormException('le texte ne doit pas dépasser les 500 caractères');
 	}
 } else {
-	CTR::$alert->add('pas assez d\'informations pour envoyer des crédits', ALERT_STD_FILLFORM);
+	throw new FormException('pas assez d\'informations pour envoyer des crédits');
 }
