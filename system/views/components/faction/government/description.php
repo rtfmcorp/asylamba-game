228df60b5ceef97bf6f2686d54fa45d816daf12a
Axel Venet <kern046@gmail.com>
2016-12-28 19:43:12 +0100

    Fix government actions

--- system/views/components/faction/government/description.php
+++ system/views/components/faction/government/description.php
@@ -3,6 +3,7 @@
 use Asylamba\Classes\Library\Format;
 
 $sessionToken = $this->getContainer()->get('app.session')->get('token');
+$colorManager = $this->getContainer()->get('demeter.color_manager');
 
 echo '<div class="component new-message">';
 	echo '<div class="head"></div>';
@@ -23,7 +24,7 @@ echo '<div class="component">';
 	echo '<div class="fix-body">';
 		echo '<div class="body">';
 			echo '<h4>Aperçu</h4>';
-			echo '<p class="long-info text-bloc">' . $faction->getParsedDescription() . '</p>';
+			echo '<p class="long-info text-bloc">' . $colorManager->getParsedDescription($faction) . '</p>';
 		echo '</div>';
 	echo '</div>';
 echo '</div>';
