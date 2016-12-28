fd0427f2471bcb2b95d8894996a8a776ea4fc8d0
Axel Venet <kern046@gmail.com>
2016-12-28 04:54:06 +0100

    Fix params settings view

--- system/views/components/params/display.php
+++ system/views/components/params/display.php
@@ -3,6 +3,9 @@
 use Asylamba\Classes\Library\Format;
 use Asylamba\Classes\Container\Params;
 
+$request = $this->getContainer()->get('app.request');
+$sessionToken = $this->getContainer()->get('app.session')->get('token');
+
 # display
 echo '<div class="component">';
 	echo '<div class="head skin-5">';
@@ -10,23 +13,23 @@ echo '<div class="component">';
 	echo '</div>';
 	echo '<div class="fix-body">';
 		echo '<div class="body">';
-			echo '<a href="' . Format::actionBuilder('switchparams', ['params' => Params::SHOW_MAP_MINIMAP]) . '"" class="on-off-button ' . (Params::check(Params::SHOW_MAP_MINIMAP) ? NULL : 'disabled') . '">';
+			echo '<a href="' . Format::actionBuilder('switchparams', $sessionToken, ['params' => Params::SHOW_MAP_MINIMAP]) . '"" class="on-off-button ' . ($request->cookies->get('p' . Params::SHOW_MAP_MINIMAP, Params::SHOW_MAP_MINIMAP) ? NULL : 'disabled') . '">';
 				echo 'Afficher la minimap';
 			echo '</a>';
 	
-			echo '<a href="' . Format::actionBuilder('switchparams', ['params' => Params::SHOW_MAP_RC]) . '"" class="on-off-button ' . (Params::check(Params::SHOW_MAP_RC) ? NULL : 'disabled') . '">';
+			echo '<a href="' . Format::actionBuilder('switchparams', $sessionToken, ['params' => Params::SHOW_MAP_RC]) . '"" class="on-off-button ' . ($request->cookies->get('p' . Params::SHOW_MAP_RC, Params::SHOW_MAP_RC) ? NULL : 'disabled') . '">';
 				echo 'Afficher les routes commerciales';
 			echo '</a>';
 
-			echo '<a href="' . Format::actionBuilder('switchparams', ['params' => Params::SHOW_MAP_ANTISPY]) . '"" class="on-off-button ' . (Params::check(Params::SHOW_MAP_ANTISPY) ? NULL : 'disabled') . '">';
+			echo '<a href="' . Format::actionBuilder('switchparams', $sessionToken, ['params' => Params::SHOW_MAP_ANTISPY]) . '"" class="on-off-button ' . ($request->cookies->get('p' . Params::SHOW_MAP_ANTISPY, Params::SHOW_MAP_ANTISPY) ? NULL : 'disabled') . '">';
 				echo 'Afficher les cercles de contre-espionnage';
 			echo '</a>';
 
-			echo '<a href="' . Format::actionBuilder('switchparams', ['params' => Params::SHOW_MAP_FLEETOUT]) . '"" class="on-off-button ' . (Params::check(Params::SHOW_MAP_FLEETOUT) ? NULL : 'disabled') . '">';
+			echo '<a href="' . Format::actionBuilder('switchparams', $sessionToken, ['params' => Params::SHOW_MAP_FLEETOUT]) . '"" class="on-off-button ' . ($request->cookies->get('p' . Params::SHOW_MAP_FLEETOUT, Params::SHOW_MAP_FLEETOUT) ? NULL : 'disabled') . '">';
 				echo 'Afficher les attaques sortantes';
 			echo '</a>';
 
-			echo '<a href="' . Format::actionBuilder('switchparams', ['params' => Params::SHOW_MAP_FLEETIN]) . '"" class="on-off-button ' . (Params::check(Params::SHOW_MAP_FLEETIN) ? NULL : 'disabled') . '">';
+			echo '<a href="' . Format::actionBuilder('switchparams', $sessionToken, ['params' => Params::SHOW_MAP_FLEETIN]) . '"" class="on-off-button ' . ($request->cookies->get('p' . Params::SHOW_MAP_FLEETIN, Params::SHOW_MAP_FLEETIN) ? NULL : 'disabled') . '">';
 				echo 'Afficher les attaques entrantes';
 			echo '</a>';
 		echo '</div>';
