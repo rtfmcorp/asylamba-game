71015c00d47abfd5c8036e42214879713c635d7c
Axel Venet <kern046@gmail.com>
2017-01-01 21:06:08 +0100

    Implement the new alert system display

--- system/views/templates/desktop/alert.php
+++ system/views/templates/desktop/alert.php
@@ -1,30 +1,15 @@
 <?php
 
-use Asylamba\Classes\Library\Http\Response;
+use Asylamba\Classes\Library\Flashbag;
 
 echo '<ul id="alert"></ul>';
 
-$response = $this->getContainer()->get('app.response');
+$session = $this->getContainer()->get('app.session');
 
-# affichage
-$redir = $response->getRedirect();
-$nbFlashbags = count($response->flashbag);
-
-if ($nbFlashbags > 0 && empty($redir)) {
-	echo '<ul id="alert-content">';
-		for ($i = 0; $i < $nbFlashbags; ++$i) {
-			$alert = $response->flashbag->get($i);
-			if (in_array($alert[1], array(Response::FLASHBAG_ERROR, Response::FLASHBAG_SUCCESS))) {
-				if (DEVMODE) {
-					echo '<li data-type="' . $alert[1] . '">';
-						echo $alert[0];
-					echo '</li>';
-				}
-			} else {
-				echo '<li data-type="' . $alert[1] . '">';
-					echo $alert[0];
-				echo '</li>';	
-			}
-		}
-	echo '</ul>';
+echo '<ul id="alert-content">';
+foreach ($session->getFlashbags() as $flashbag) {
+	echo "<li data-type='{$flashbag->getType()}'>{$flashbag->getMessage()}</li>";
 }
+echo '</ul>';
+
+$session->flushFlashbags();
