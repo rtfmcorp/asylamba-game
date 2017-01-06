3000b099ad267ba7e2d438a514bb9416b964894c
Axel Venet <kern046@gmail.com>
2017-01-06 07:24:47 +0100

    Create CommercialRoute repository

--- system/Modules/Athena/Manager/CommercialRouteManager.php
+++ system/Modules/Athena/Manager/CommercialRouteManager.php
@@ -11,193 +11,154 @@
 */
 namespace Asylamba\Modules\Athena\Manager;
 
-use Asylamba\Classes\Worker\Manager;
-use Asylamba\Classes\Library\Utils;
-use Asylamba\Classes\Database\Database;
+use Asylamba\Classes\Entity\EntityManager;
 use Asylamba\Modules\Athena\Model\CommercialRoute;
 use Asylamba\Modules\Demeter\Model\Color;
+use Asylamba\Modules\Athena\Model\OrbitalBase;
 
-class CommercialRouteManager extends Manager {
-	protected $managerType = '_CommercialRoute';
-
+class CommercialRouteManager {
+	/** @var EntityManager **/
+	protected $entityManager;
+	
 	/**
-	 * @param Database $database
+	 * @param EntityManager $entityManager
 	 */
-	public function __construct(Database $database) {
-		parent::__construct($database);
+	public function __construct(EntityManager $entityManager) {
+		$this->entityManager = $entityManager;
 	}
 	
-	//METHODS
-	public function load($where, $order = array(), $limit = array()) {
-		$formatWhere = Utils::arrayToWhere($where, 'cr.');
-		$formatOrder = Utils::arrayToOrder($order);
-		$formatLimit = Utils::arrayToLimit($limit);
-
-		$qr = $this->database->prepare('SELECT 
-			cr.id AS id,
-			cr.rOrbitalBase AS rOrbitalBase,
-			cr.rOrbitalBaseLinked AS rOrbitalBaseLinked,
-			cr.imageLink AS imageLink,
-			cr.distance AS distance,
-			cr.price AS price,
-			cr.income AS income,
-			cr.dProposition AS dProposition,
-			cr.dCreation AS dCreation,
-			cr.statement AS statement,
-
-			ob1.rPlayer AS playerId1,
-			ob1.name AS baseName1,
-			ob1.typeOfBase AS baseType1,
-			pl1.name AS playerName1,
-			pl1.rColor AS playerColor1,
-			pl1.avatar AS avatar1,
-			p1.population AS population1,
-
-			ob2.rPlayer AS playerId2,
-			ob2.name AS baseName2,
-			ob2.typeOfBase AS baseType2,
-			pl2.name AS playerName2,
-			pl2.rColor AS playerColor2,
-			pl2.avatar AS avatar2,
-			p2.population AS population2
-
-			FROM commercialRoute AS cr
-
-			LEFT JOIN orbitalBase AS ob1
-			ON cr.rOrbitalBase = ob1.rPlace
-			LEFT JOIN player AS pl1
-			ON ob1.rPlayer = pl1.id
-			LEFT JOIN place AS p1
-			ON ob1.rPlace = p1.id
-
-			LEFT JOIN orbitalBase AS ob2
-			ON cr.rOrbitalBaseLinked = ob2.rPlace
-			LEFT JOIN player AS pl2
-			ON ob2.rPlayer = pl2.id
-			LEFT JOIN place AS p2
-			ON ob2.rPlace = p2.id
-
-			' . $formatWhere . '
-			' . $formatOrder . '
-			' . $formatLimit
-		);
-
-		foreach($where AS $v) {
-			if (is_array($v)) {
-				foreach ($v as $p) {
-					$valuesArray[] = $p;
-				}
-			} else {
-				$valuesArray[] = $v;
-			}
-		}
-
-		if(empty($valuesArray)) {
-			$qr->execute();
-		} else {
-			$qr->execute($valuesArray);
-		}
-
-		while($aw = $qr->fetch()) {
-			$cr = new CommercialRoute();
-			$cr->setId($aw['id']);
-			$cr->setROrbitalBase($aw['rOrbitalBase']);
-			$cr->setROrbitalBaseLinked($aw['rOrbitalBaseLinked']);
-			$cr->setImageLink($aw['imageLink']);
-			$cr->setDistance($aw['distance']);
-			$cr->setPrice($aw['price']);
-			$cr->setIncome($aw['income']);
-			$cr->setDProposition($aw['dProposition']);
-			$cr->setDCreation($aw['dCreation']);
-			$cr->setStatement($aw['statement']);
-
-			$cr->setBaseName1($aw['baseName1']);
-			$cr->baseType1 = $aw['baseType1'];
-			$cr->setPlayerId1($aw['playerId1']);
-			$cr->setPlayerName1($aw['playerName1']);
-			$cr->playerColor1 = $aw['playerColor1'];
-			$cr->setAvatar1($aw['avatar1']);
-			$cr->setPopulation1($aw['population1']);
-
-			$cr->setBaseName2($aw['baseName2']);
-			$cr->baseType2 = $aw['baseType2'];
-			$cr->setPlayerId2($aw['playerId2']);
-			$cr->setPlayerName2($aw['playerName2']);
-			$cr->playerColor2 = $aw['playerColor2'];
-			$cr->setAvatar2($aw['avatar2']);
-			$cr->setPopulation2($aw['population2']);
-
-			$this->_Add($cr);
-		}
+	/**
+	 * @param int $id
+	 * @return CommercialRoute
+	 */
+	public function get($id)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->get($id);
+	}
+	
+	/**
+	 * @param int $id
+	 * @param int $baseId
+	 * @return CommercialRoute
+	 */
+	public function getByIdAndBase($id, $baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getByIdAndBase($id, $baseId);
+	}
+	
+	/**
+	 * @param int $id
+	 * @param int $baseId
+	 * @return CommercialRoute
+	 */
+	public function getByIdAndDistantBase($id, $baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getByIdAndDistantBase($id, $baseId);
 	}
 
-	public function emptySession() {
-		# empty the session, for player rankings
-		$this->_EmptyCurrentSession();
-		$this->newSession(FALSE);
+	/**
+	 * @param int $baseId
+	 * @return array
+	 */
+	public function getByBase($baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getByBase($baseId);
+	}
+	
+	/**
+	 * @param int $baseId
+	 * @return array
+	 */
+	public function getByDistantBase($baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getByDistantBase($baseId);
+	}
+	
+	/**
+	 * @param int $baseId
+	 * @param int $distantBaseId
+	 * @return CommercialRoute
+	 */
+	public function getExistingRoute($baseId, $distantBaseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getExistingRoute($baseId, $distantBaseId);
+	}
+	
+	public function getBaseIncome(OrbitalBase $orbitalBase)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->getBaseIncome($orbitalBase->getId());
+	}
+	
+	/**
+	 * @param int $baseId
+	 * @param int $distantBaseId
+	 * @return bool
+	 */
+	public function isAlreadyARoute($baseId, $distantBaseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->isAlreadyARoute($baseId, $distantBaseId);
+	}
+	
+	/**
+	 * @param int $baseId
+	 * @return int
+	 */
+	public function countBaseRoutes($baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->countBaseRoutes($baseId);
+	}
+	
+	/**
+	 * @param int $baseId
+	 * @return int
+	 */
+	public function countBaseActiveAndStandbyRoutes($baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->countBaseActiveAndStandbyRoutes($baseId);
+	}
+	
+	
+	/**
+	 * @param int $baseId
+	 * @return int
+	 */
+	public function countBaseActiveRoutes($baseId)
+	{
+		return $this->entityManager->getRepository(CommercialRoute::class)->countBaseActiveRoutes($baseId);
+	}
+	
+	/**
+	 * @param CommercialRoute $commercialRoute
+	 */
+	public function add(CommercialRoute $commercialRoute) {
+		$this->entityManager->persist($commercialRoute);
+		$this->entityManager->flush();
 	}
 
-	public function add(CommercialRoute $cr) {
-		$qr = $this->database->prepare('INSERT INTO
-			commercialRoute(rOrbitalBase, rOrbitalBaseLinked, imageLink, distance, price, income, dProposition, dCreation, statement)
-			VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)');
-		$qr->execute(array(
-			$cr->getROrbitalBase(),
-			$cr->getROrbitalBaseLinked(),
-			$cr->getImageLink(),
-			$cr->getDistance(),
-			$cr->getPrice(),
-			$cr->getIncome(),
-			$cr->getDProposition(),
-			$cr->getDCreation(),
-			$cr->getStatement()
-		));
-
-		$cr->setId($this->database->lastInsertId());
-		$this->_Add($cr);
+	/**
+	 * @param CommercialRoute $commercialRoute
+	 */
+	public function remove(CommercialRoute $commercialRoute) {
+		$this->entityManager->remove($commercialRoute);
+		$this->entityManager->flush();
 	}
-
-	public function save() {
-		$routes = $this->_Save();
-		foreach ($routes AS $k => $cr) {
-			$qr = $this->database->prepare('UPDATE commercialRoute
-				SET id = ?,
-					rOrbitalBase = ?,
-					rOrbitalBaseLinked = ?,
-					imageLink = ?,
-					distance = ?,
-					price = ?,
-					income = ?,
-					dProposition = ?,
-					dCreation = ?,
-					statement = ?
-				WHERE id = ?
-			');
-			$qr->execute(array(
-				$cr->getId(),
-				$cr->getROrbitalBase(), 
-				$cr->getROrbitalBaseLinked(), 
-				$cr->getImageLink(), 
-				$cr->getDistance(), 
-				$cr->getPrice(), 
-				$cr->getIncome(), 
-				$cr->getDProposition(),
-				$cr->getDCreation(), 
-				$cr->getStatement(), 
-				$cr->getId()
-			));
-		}
-	}
-
-	public function deleteById($id) {
-		$qr = $this->database->prepare('DELETE FROM commercialRoute WHERE id = ?');
-		$qr->execute(array($id));
+	
+	public function removeBaseRoutes(OrbitalBase $orbitalBase)
+	{
+		$repository = $this->entityManager->getRepository(CommercialRoute::class);
 		
-		// suppression de l'objet du manager
-		$this->_Remove($id);
-		return TRUE;
+		$routes = array_merge(
+			$repository->getByBase($orbitalBase->getId()),
+			$repository->getByDistantBase($orbitalBase->getId())
+		);
+		foreach($routes as $route) {
+			$this->entityManager->remove($route);
+			// @TODO notifications
+		}
+		$this->entityManager->flush();
 	}
 
+	// @TODO
 	public function freezeRoute($color1, $color2) {
 		$freeze = TRUE;
 		if (!($color1->colorLink[$color2->id] == Color::ENEMY || $color2->colorLink[$color1->id] == Color::ENEMY)) {
