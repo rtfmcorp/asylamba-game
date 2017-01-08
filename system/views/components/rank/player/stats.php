846978c722cb31a1edf221d2eb193bfd5204f694
Axel Venet <kern046@gmail.com>
2017-01-08 14:45:58 +0100

    Fix ranking player counts with new manager methods

--- system/views/components/rank/player/stats.php
+++ system/views/components/rank/player/stats.php
@@ -2,7 +2,6 @@
 # require
 
 use Asylamba\Classes\Library\Format;
-use Asylamba\Modules\Zeus\Model\Player;
 
 $playerManager = $this->getContainer()->get('zeus.player_manager');
 
@@ -14,13 +13,13 @@ echo '<div class="component player rank">';
 
 			echo '<div class="number-box">';
 				echo '<span class="label">Joueurs actifs</span>';
-				echo '<span class="value">' . Format::number($playerManager->count(array('statement' => Player::ACTIVE))) . '</span>';
+				echo '<span class="value">' . Format::number($playerManager->countActivePlayers()) . '</span>';
 				echo '<span class="group-link"><a href="#" title="compte tous les joueurs qui se sont connectés depuis 15 jours" class="hb lt">?</a></span>';
 			echo '</div>';
 
 			echo '<div class="number-box">';
 				echo '<span class="label">Joueurs inscrits</span>';
-				echo '<span class="value">' . Format::number($playerManager->count(array('statement' => array(Player::ACTIVE, Player::INACTIVE)))) . '</span>';
+				echo '<span class="value">' . Format::number($playerManager->countAllPlayers()) . '</span>';
 			echo '</div>';
 
 			echo '<p>Le classement est mis à jour tous les jours à 3h (UTC+1) du matin.</p>';
