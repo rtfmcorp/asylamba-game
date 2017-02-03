56f26ee738b1e945bea2467acb31e67863a062e6
Axel Venet <kern046@gmail.com>
2017-02-03 09:47:11 +0100

    Implement new unit of work for factions

--- system/Modules/Demeter/Manager/ColorManager.php
+++ system/Modules/Demeter/Manager/ColorManager.php
@@ -14,9 +14,7 @@ namespace Asylamba\Modules\Demeter\Manager;
 
 use Asylamba\Classes\Entity\EntityManager;
 use Asylamba\Classes\Worker\CTC;
-use Asylamba\Classes\Worker\Manager;
 use Asylamba\Classes\Library\Utils;
-use Asylamba\Classes\Database\Database;
 use Asylamba\Modules\Demeter\Model\Color;
 use Asylamba\Modules\Demeter\Model\Law\Law;
 use Asylamba\Modules\Hermes\Model\Notification;
@@ -40,9 +38,7 @@ use Asylamba\Modules\Demeter\Model\Election\Election;
 use Asylamba\Classes\Library\Parser;
 use Asylamba\Classes\Library\Format;
 
-class ColorManager extends Manager {
-	/** @var string **/
-	protected $managerType ='_Color';
+class ColorManager {
 	/** @var EntityManager **/
 	protected $entityManager;
 	/** @var PlayerManager **/
@@ -73,7 +69,6 @@ class ColorManager extends Manager {
 	protected $ctc;
 	
 	/**
-	 * @param Database $database
 	 * @param EntityManager $entityManager
 	 * @param PlayerManager $playerManager
 	 * @param VoteManager $voteManager
@@ -90,7 +85,6 @@ class ColorManager extends Manager {
 	 * @param CTC $ctc
 	 */
 	public function __construct(
-		Database $database,
 		EntityManager $entityManager,
 		PlayerManager $playerManager,
 		VoteManager $voteManager,
@@ -106,7 +100,6 @@ class ColorManager extends Manager {
 		Parser $parser,
 		CTC $ctc
 	) {
-		parent::__construct($database);
 		$this->entityManager = $entityManager;
 		$this->playerManager = $playerManager;
 		$this->voteManager = $voteManager;
@@ -124,213 +117,64 @@ class ColorManager extends Manager {
 	}
 	
 	/**
+	 * @param int $id
+	 * @return Color
+	 */
+	public function get($id)
+	{
+		return $this->entityManager->getRepository(Color::class)->get($id);
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getAll()
+	{
+		return $this->entityManager->getRepository(Color::class)->getAll();
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getInGameFactions()
+	{
+		return $this->entityManager->getRepository(Color::class)->getInGameFactions();
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getOpenFactions()
+	{
+		return $this->entityManager->getRepository(Color::class)->getOpenFactions();
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function getAllByActivePlayersNumber()
+	{
+		return $this->entityManager->getRepository(Color::class)->getAllByActivePlayersNumber();
+	}
+
+	/**
+	 * @param Color $faction
+	 * @return int
+	 */
+	public function add(Color $faction) {
+		$this->entityManager->persist($faction);
+		$this->entityManager->flush($faction);
+
+		return $faction->id;
+	}
+	
+	/**
 	 * @param Color $color
 	 * @return string
 	 */
 	public function getParsedDescription(Color $color) {
 		$this->parser->parseBigTag = TRUE;
 		return $this->parser->parse($color->description);
-	}
-	
-	public function load($where = array(), $order = array(), $limit = array()) {
-		$formatWhere = Utils::arrayToWhere($where, 'c.');
-		$formatOrder = Utils::arrayToOrder($order);
-		$formatLimit = Utils::arrayToLimit($limit);
-		
-		$qr = $this->database->prepare('SELECT c.*
-			FROM color AS c
-			' . $formatWhere .'
-			' . $formatOrder .'
-			' . $formatLimit
-		);
-
-		foreach($where AS $v) {
-			if (is_array($v)) {
-				foreach ($v as $p) {
-					$valuesArray[] = $p;
-				}
-			} else {
-				$valuesArray[] = $v;
-			}
-		}
-
-		if (empty($valuesArray)) {
-			$qr->execute();
-		} else {
-			$qr->execute($valuesArray);
-		}
-
-		$awColor = $qr->fetchAll();
-
-		$qr->closeCursor();
-
-		$qr = $this->database->prepare('SELECT c.*
-			FROM colorLink AS c ORDER BY rColorLinked'
-		);
-
-		$qr->execute();
-
-		$awColorLink = $qr->fetchAll();
-		$qr->closeCursor();
-
-		$colorsArray = array();
-
-		for ($i = 0; $i < count($awColor); $i++) {
-			$color = new Color();
-			$color->id = $awColor[$i]['id'];
-			$color->alive = $awColor[$i]['alive'];
-			$color->isWinner = $awColor[$i]['isWinner'];
-			$color->credits = $awColor[$i]['credits'];
-			$color->players = $awColor[$i]['players'];
-			$color->activePlayers = $awColor[$i]['activePlayers'];
-			$color->rankingPoints = $awColor[$i]['rankingPoints'];
-			$color->points = $awColor[$i]['points'];
-			$color->sectors = $awColor[$i]['sectors'];
-			$color->electionStatement = $awColor[$i]['electionStatement'];
-			$color->isClosed = $awColor[$i]['isClosed'];
-			$color->description = $awColor[$i]['description'];
-			$color->dClaimVictory = $awColor[$i]['dClaimVictory'];
-			$color->dLastElection = $awColor[$i]['dLastElection'];
-			$color->isInGame = $awColor[$i]['isInGame'];
-			$color->colorLink[0] = Color::NEUTRAL;
-
-			$color->officialName = ColorResource::getInfo($color->id, 'officialName');
-			$color->popularName = ColorResource::getInfo($color->id, 'popularName');
-			$color->government = ColorResource::getInfo($color->id, 'government');
-			$color->demonym = ColorResource::getInfo($color->id, 'demonym');
-			$color->factionPoint = ColorResource::getInfo($color->id, 'factionPoint');
-			$color->status = ColorResource::getInfo($color->id, 'status');
-			$color->regime = ColorResource::getInfo($color->id, 'regime');
-			$color->devise = ColorResource::getInfo($color->id, 'devise');
-			$color->desc1 = ColorResource::getInfo($color->id, 'desc1');
-			$color->desc2 = ColorResource::getInfo($color->id, 'desc2');
-			$color->desc3 = ColorResource::getInfo($color->id, 'desc3');
-			$color->desc4 = ColorResource::getInfo($color->id, 'desc4');
-			$color->bonus = ColorResource::getInfo($color->id, 'bonus');
-			$color->mandateDuration = ColorResource::getInfo($color->id, 'mandateDuration');
-			$color->senateDesc = ColorResource::getInfo($color->id, 'senateDesc');
-			$color->campaignDesc = ColorResource::getInfo($color->id, 'campaignDesc');
-	
-			$color->bonusText = [];
-			foreach (ColorResource::getInfo($color->id, 'bonus') AS $k) {
-				$color->bonusText[] = ColorResource::getBonus($k);
-			}
-
-			if ($color->id != 0) {
-				foreach ($awColorLink AS $colorLink) {
-					if ($colorLink['rColor'] == $color->id) {
-						$color->colorLink[$colorLink['rColorLinked']] = $colorLink['statement'];
-					}
-				}
-			} else {
-				$color->colorLink = array(Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL, Color::NEUTRAL);
-			}
-
-			$this->_Add($color);
-			if ($this->currentSession->getUMode()) {
-				$this->uMethod($color);
-			}
-		}
-	}
-
-	public function save() {
-		$colors = $this->_Save();
-
-		foreach ($colors AS $color) {
-			$qr = $this->database->prepare('UPDATE color
-				SET
-					alive = ?,
-					isWinner = ?,
-					credits = ?,
-					players = ?,	
-					activePlayers = ?,
-					rankingPoints = ?,
-					points = ?,
-					sectors = ?,
-					electionStatement = ?,
-					isClosed = ?,
-					isInGame = ?,
-					description = ?,
-					dClaimVictory = ?,
-					dLastElection = ?
-				WHERE id = ?');
-			$aw = $qr->execute(array(
-					$color->alive,
-					$color->isWinner,
-					$color->credits,
-					$color->players,
-					$color->activePlayers,
-					$color->rankingPoints,
-					$color->points,
-					$color->sectors,
-					$color->electionStatement,
-					$color->isClosed,
-					$color->isInGame,
-					$color->description,
-					$color->dClaimVictory,
-					$color->dLastElection,
-					$color->id
-				));
-
-			$qr2 = $this->database->prepare('UPDATE colorLink SET
-					statement = ? WHERE rColor = ? AND rColorLinked = ?
-				');
-
-			foreach ($color->colorLink as $key => $value) {
-				$qr2->execute(array($value, $color->id, $key));
-			}
-		}
-	}
-
-	public function add($newColor) {
-		$qr = $this->database->prepare('INSERT INTO color
-		SET
-			id = ?,
-			alive = ?,
-			isWinner = ?,
-			credits = ?,
-			players = ?,		
-			activePlayers = ?,
-			rankingPoints = ?,
-			points = ?,
-			sectors = ?,
-			electionStatement = ?,
-			isClosed = ?,
-			isInGame = ?,
-			description = ?,
-			dClaimVictory = ?,
-			dLastElection = ?');
-		$aw = $qr->execute(array(
-				$color->id,
-				$color->alive,
-				$color->isWinner,
-				$color->credits,
-				$color->players,
-				$color->activePlayers,
-				$color->rankingPoints,
-				$color->points,
-				$color->sectors,
-				$color->electionStatement,
-				$color->isClosed,
-				$color->isInGame,
-				$color->description,
-				$color->dClaimVictory,
-				$color->dLastElection
-			));
-
-		$newColor->id = $this->database->lastInsertId();
-
-		$this->_Add($newColor);
-
-		return $newColor->id;
-	}
-
-	public function deleteById($id) {
-		$qr = $this->database->prepare('DELETE FROM color WHERE id = ?');
-		$qr->execute(array($id));
-
-		$this->_Remove($id);
-		return TRUE;
 	}
 
 	// FONCTIONS STATICS
