9bde6e0be4bdebec34a91f390de610f44574a9b7
Kern <kern046@gmail.com>
2017-03-12 03:21:02 +0100

    Replace deprecated mcrypt with openssl

--- system/Classes/Library/Security.php
+++ system/Classes/Library/Security.php
@@ -4,34 +4,19 @@ namespace Asylamba\Classes\Library;
 
 class Security {
 	public function crypt($query, $key) {
-		$cipher = mcrypt_module_open('rijndael-256', '', 'ecb', '');
-		$ivSize = mcrypt_enc_get_iv_size($cipher);
-		$iv = mcrypt_create_iv($ivSize, MCRYPT_RAND);
+		$iv = openssl_random_pseudo_bytes(16);
 
-		mcrypt_generic_init($cipher, $key, $iv);
+		$cipher = openssl_encrypt($query,  'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv);
 
-		$data = mcrypt_generic($cipher, $query);
-		$data = rtrim(strtr(base64_encode($data), '+/', '~_'), '=');
-
-		mcrypt_generic_deinit($cipher);
-
-		return $data;
+		return rtrim(strtr(base64_encode($cipher), '+/', '~_'), '=');
 	}
 
 	public function uncrypt($query, $key) {
-		$cipher = mcrypt_module_open('rijndael-256', '', 'ecb', '');
-		$ivSize = mcrypt_enc_get_iv_size($cipher);
-		$iv = mcrypt_create_iv($ivSize, MCRYPT_RAND);
-
-		mcrypt_generic_init($cipher, $key, $iv);
-
-		$crypted = base64_decode(str_pad(strtr($query, '~_', '+/'), strlen($query) % 4, '=', STR_PAD_RIGHT));
-		$decrypted = trim(mdecrypt_generic($cipher, $crypted));
-
-		mcrypt_generic_deinit($cipher);
-		mcrypt_module_close($cipher);
-
-		return $decrypted;
+		$iv = openssl_random_pseudo_bytes(16);
+		
+		$cipher = base64_decode(str_pad(strtr($query, '~_', '+/'), strlen($query) % 4, '=', STR_PAD_RIGHT));
+		
+		return trim(openssl_decrypt($cipher, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $iv));
 	}
 
 	public function buildBindkey($bindkey) {
