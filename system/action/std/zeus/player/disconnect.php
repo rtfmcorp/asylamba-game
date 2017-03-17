b7f096c4573067e77a3566a6e6a459844163c657
Axel Venet <kern046@gmail.com>
2017-03-17 20:01:30 +0100

    Fix log out redirection location

--- system/action/std/zeus/player/disconnect.php
+++ system/action/std/zeus/player/disconnect.php
@@ -1,4 +1,4 @@
 <?php
 
 $this->getContainer()->get('app.session')->destroy();
-$this->getContainer()->get('app.response')->redirect(GETOUT_ROOT . 'profil', TRUE);
+$this->getContainer()->get('app.response')->redirect('profil', TRUE);
