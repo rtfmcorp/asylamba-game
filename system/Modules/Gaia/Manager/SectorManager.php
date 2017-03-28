07a506271d1d84149becac0124085629827eb9b2
Axel Venet <kern046@gmail.com>
2017-03-29 00:28:08 +0200

    Implement repository for sectors

--- system/Modules/Gaia/Manager/SectorManager.php
+++ system/Modules/Gaia/Manager/SectorManager.php
@@ -12,92 +12,39 @@
 namespace Asylamba\Modules\Gaia\Manager;
 
 use Asylamba\Classes\Library\Utils;
-use Asylamba\Classes\Database\Database;
-use Asylamba\Classes\Worker\Manager;
+
+use Asylamba\Classes\Entity\EntityManager;
+
 use Asylamba\Modules\Gaia\Model\Sector;
 
-class SectorManager extends Manager {
-	protected $managerType = '_Sector';
-
-	private $sectors = array();
+class SectorManager {
+	/** @var EntityManager **/
+	protected $entityManager;
 	
 	/**
-	 * @param Database $database
+	 * @param EntityManager $entityManager
 	 */
-	public function __construct(Database $database) {
-		parent::__construct($database);
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->entityManager = $entityManager;
 	}
 
-	public function get($position = 0) {
-		if (isset($this->sectors[$position][0])) {
-			$this->sectors[$position][1] = FALSE;
-			return $this->sectors[$position][0];
-		} else {
-			return FALSE;
-		}
+	/**
+	 * @param int $id
+	 * @return Sector
+	 */
+	public function get($id) {
+		return $this->entityManager->getRepository(Sector::class)->get($id);
 	}
-
-	public function getById($id) {
-		foreach ($this->sectors as $k => $v) {
-			if ($v[0]->getId() == $id) {
-				$this->sectors[$k][1] = FALSE;
-				return $this->sectors[$k][0];
-			}
-		}
-		return FALSE;
+	
+	public function getFactionSectors($factionId)
+	{
+		return $this->entityManager->getRepository(Sector::class)->getFactionSectors($factionId);
 	}
-
-	public function load($where = array(), $order = array(), $limit = array()) {
-		$formatWhere = Utils::arrayToWhere($where);
-		$formatOrder = Utils::arrayToOrder($order);
-		$formatLimit = Utils::arrayToLimit($limit);
-
-		$qr = $this->database->prepare('SELECT *
-			FROM sector
-			' . $formatWhere . '
-			' . $formatOrder . '
-			' . $formatLimit);
-
-		foreach ($where AS $v) {
-			$valuesArray[] = $v;
-		}
-
-		if (empty($valuesArray)) {
-			$qr->execute();
-		} else {
-			$qr->execute($valuesArray);
-		}
-
-		$answer = $qr->fetchAll();
-		if (!empty($answer)) {
-			$k = 1;
-
-			foreach ($answer as $aw) {
-				$sector = new Sector();
-
-				$sector->setId($aw['id']);
-				$sector->setRColor($aw['rColor']);
-				$sector->rSurrender = $aw['rSurrender'];
-				$sector->setXPosition($aw['xPosition']);
-				$sector->setYPosition($aw['yPosition']);
-				$sector->setXBarycentric($aw['xBarycentric']);
-				$sector->setYBarycentric($aw['yBarycentric']);
-				$sector->setTax($aw['tax']);
-				$sector->setName($aw['name']);
-				$sector->setPoints($aw['points']);
-				$sector->setPopulation($aw['population']);
-				$sector->setLifePlanet($aw['lifePlanet']);
-
-				$this->sectors[] = array($sector, TRUE);
-
-				$k++;
-
-				$this->_Add($sector);
-			}
-			return TRUE;
-		} else {
-			return FALSE;
-		}
+	
+	public function getAll()
+	{
+		return $this->entityManager->getRepository(Sector::class)->getAll();
 	}
 
 	public function loadByOr($module, $list = array()) {
@@ -145,27 +92,5 @@ class SectorManager extends Manager {
 		} else {
 			return FALSE;
 		}
-	}
-
-	public function save() {
-		$sectors = $this->_Save();
-		foreach ($sectors AS $s) {
-			$qr = $this->database->prepare('UPDATE sector
-				SET
-					rSurrender = ?,
-					tax = ?,
-					name = ?
-				WHERE id = ?');
-			$qr->execute(array(
-				$s->rSurrender,
-				$s->tax,
-				$s->name,
-				$s->id
-			));
-		}
-	}
-
-	public function size() {
-		return count($this->sectors);
 	}
 }
