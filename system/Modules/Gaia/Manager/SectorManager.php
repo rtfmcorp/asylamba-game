a6bdde248d48bf200b3b4964f2a9de3a867c1937
Axel Venet <kern046@gmail.com>
2017-03-29 01:01:08 +0200

    Remove useless load method

--- system/Modules/Gaia/Manager/SectorManager.php
+++ system/Modules/Gaia/Manager/SectorManager.php
@@ -11,8 +11,6 @@
 */
 namespace Asylamba\Modules\Gaia\Manager;
 
-use Asylamba\Classes\Library\Utils;
-
 use Asylamba\Classes\Entity\EntityManager;
 
 use Asylamba\Modules\Gaia\Model\Sector;
@@ -37,60 +35,20 @@ class SectorManager {
 		return $this->entityManager->getRepository(Sector::class)->get($id);
 	}
 	
+	/**
+	 * @param int $factionId
+	 * @return array
+	 */
 	public function getFactionSectors($factionId)
 	{
 		return $this->entityManager->getRepository(Sector::class)->getFactionSectors($factionId);
 	}
 	
+	/**
+	 * @return array
+	 */
 	public function getAll()
 	{
 		return $this->entityManager->getRepository(Sector::class)->getAll();
-	}
-
-	public function loadByOr($module, $list = array()) {
-		$query = '';
-		foreach ($list as $v) { $query .= $module . ' = ? OR '; }
-		$query = trim($query, 'OR ');
-
-		$qr = $this->database->prepare('SELECT 
-			DISTINCT(se.id),
-			se.*
-			FROM sector AS se
-			LEFT JOIN system AS sy
-				ON sy.rSector = se.id
-			LEFT JOIN place AS pl
-				ON pl.rSystem = sy.id
-			WHERE ' . $query);
-
-		if (empty($list)) {
-			$qr->execute();
-		} else {
-			$qr->execute($list);
-		}
-
-		$aw = $qr->fetchAll();
-		if (!empty($aw)) {
-			foreach ($aw as $s) {
-				$sector = new Sector();
-
-				$sector->setId($s['id']);
-				$sector->setRColor($s['rColor']);
-				$sector->rSurrender = $s['rSurrender'];
-				$sector->setXPosition($s['xPosition']);
-				$sector->setYPosition($s['yPosition']);
-				$sector->setXBarycentric($aw['xBarycentric']);
-				$sector->setYBarycentric($aw['yBarycentric']);
-				$sector->setTax($s['tax']);
-				$sector->setName($s['name']);
-				$sector->setPoints($s['points']);
-				$sector->setPopulation($s['population']);
-				$sector->setLifePlanet($s['lifePlanet']);
-
-				$this->sectors[] = array($sector, TRUE);
-			}
-			return TRUE;
-		} else {
-			return FALSE;
-		}
 	}
 }
