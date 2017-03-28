2c1de085648777511397b622cbe43596d7b12a4d
Axel Venet <kern046@gmail.com>
2017-03-28 09:09:57 +0200

    Implement a request factory to contain the server input

--- system/Classes/Library/Http/Request.php
+++ system/Classes/Library/Http/Request.php
@@ -6,77 +6,102 @@ use Asylamba\Classes\Library\ParameterBag;
 use Asylamba\Classes\Container\Cookie;
 
 class Request {
+    /** @var string **/
+    protected $method;
+    /** @var string **/
+    protected $protocol;
+    /** @var boolean **/
+    protected $isCrossDomain = false;
+    /** @var string **/
+    protected $externalDomain;
+    /** @var string **/
+    protected $redirect;
 	/** @var string **/
-	protected $url;
-	/** @var boolean **/
-	protected $isCrossDomain = false;
-	/** @var string **/
-	protected $externalDomain;
-	/** @var string **/
-	protected $redirect;
-	/** @var ParameterBag **/
-	public $headers;
-	/** @var ParameterBag **/
-	public $request;
-	/** @var ParameterBag **/
-	public $query;
-	/** @var Cookie **/
-	public $cookies;
-	
-	public function __construct()
-	{
-		$this->headers = new ParameterBag();
-		$this->request = new ParameterBag();
-		$this->query = new ParameterBag();
-		$this->cookies = new Cookie();
-	}
-	
-	public function initialize()
-	{
-		foreach ($_GET as $key => $value) {
-			$this->query->set($key, $value);
-		}
-		foreach ($_POST as $key => $value) {
-			$this->request->set($key, $value);
-		}
-		foreach ($_SERVER as $key => $value) {
-			if (strpos($key, 'HTTP_') === false) {
-				continue;
-			}
-			$this->headers->set(str_replace('_', '-', strtolower(substr($key, 5))), $value);
-		}
-	}
+	protected $path;
+    /** @var ParameterBag **/
+    public $headers;
+    /** @var ParameterBag **/
+    public $request;
+    /** @var ParameterBag **/
+    public $query;
+    /** @var Cookie **/
+    public $cookies;
+    /** @var string **/
+    public $body = '';
 
-	/**
-	 * @param string $url
-	 */
-	public function setUrl($url)
-	{
-		$this->url = $url;
-	}
-	
-	/**
-	 * @return string
-	 */
-	public function getUrl()
-	{
-		return $this->url;
-	}
-	
-	/**
-	 * @param string $externalDomain
-	 * @return \Asylamba\Classes\Library\Http\Request
-	 */
-	public function setCrossDomain($externalDomain)
-	{
-		$this->externalDomain = $externalDomain;
-		$this->isCrossDomain = true;
-		
-		return $this;
-	}
-	
-	public function getCrossDomain()
-	{
-		return $this->isCrossDomain;
-	}
+    public function __construct()
+    {
+        $this->headers = new ParameterBag();
+        $this->request = new ParameterBag();
+        $this->query = new ParameterBag();
+        $this->cookies = new Cookie();
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param string $protocol
+     */
+    public function setProtocol($protocol)
+    {
+        $this->protocol = $protocol;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProtocol()
+    {
+        return $this->protocol;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $externalDomain
+     * @return \Asylamba\Classes\Library\Http\Request
+     */
+    public function setCrossDomain($externalDomain)
+    {
+        $this->externalDomain = $externalDomain;
+        $this->isCrossDomain = true;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getCrossDomain()
+    {
+        return $this->isCrossDomain;
+    }
 }
