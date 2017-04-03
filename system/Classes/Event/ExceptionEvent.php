a7e435614d0d59b3ca9832e6e15e0a1d2f545176
Axel Venet <kern046@gmail.com>
2017-04-03 13:19:57 +0200

    Allow exception listener to update the server response

--- system/Classes/Event/ExceptionEvent.php
+++ system/Classes/Event/ExceptionEvent.php
@@ -2,18 +2,35 @@
 
 namespace Asylamba\Classes\Event;
 
+use Asylamba\Classes\Library\Http\Request;
+use Asylamba\Classes\Library\Http\Response;
+
 class ExceptionEvent {
 	/** @var \Exception **/
 	protected $exception;
+	/** @var Request **/
+	protected $request;
+	/** @var Response **/
+	protected $response;
 	
 	const NAME = 'core.exception';
 	
 	/**
+	 * @param Request $request
 	 * @param \Exception $exception
 	 */
-	public function __construct(\Exception $exception)
+	public function __construct(Request $request, \Exception $exception)
 	{
+		$this->request = $request;
 		$this->exception = $exception;
+	}
+	
+	/**
+	 * @return Request
+	 */
+	public function getRequest()
+	{
+		return $this->request;
 	}
 	
 	/**
@@ -22,5 +39,24 @@ class ExceptionEvent {
 	public function getException()
 	{
 		return $this->exception;
+	}
+	
+	/**
+	 * @param Response $response
+	 * @return \Asylamba\Classes\Event\ErrorEvent
+	 */
+	public function setResponse(Response $response)
+	{
+		$this->response = $response;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return Response
+	 */
+	public function getResponse()
+	{
+		return $this->response;
 	}
 }
