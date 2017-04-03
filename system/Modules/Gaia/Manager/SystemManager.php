50f1e0b4aa5590de70c8fd04a127a3595abaefd8
Axel Venet <kern046@gmail.com>
2017-04-03 13:25:59 +0200

    Implement repository for systems

--- system/Modules/Gaia/Manager/SystemManager.php
+++ system/Modules/Gaia/Manager/SystemManager.php
@@ -11,77 +11,27 @@
 */
 namespace Asylamba\Modules\Gaia\Manager;
 
-use Asylamba\Classes\Worker\Manager;
-use Asylamba\Classes\Database\Database;
-use Asylamba\Classes\Library\Utils;
+use Asylamba\Classes\Entity\EntityManager;
 
 use Asylamba\Modules\Gaia\Model\System;
 
-class SystemManager extends Manager {
-	protected $managerType = '_System';
+class SystemManager {
+	/** @var EntityManager **/
+	protected $entityManager;
 
 	/**
-	 * @param Database $database
+	 * @param EntityManager $entityManager
 	 */
-	public function __construct(Database $database) {
-		parent::__construct($database);
+	public function __construct(EntityManager $entityManager) {
+		$this->entityManager = $entityManager;
 	}
 	
-	public function load($where = array(), $order = array(), $limit = array()) {
-		$formatWhere = Utils::arrayToWhere($where, 's.');
-		$formatOrder = Utils::arrayToOrder($order);
-		$formatLimit = Utils::arrayToLimit($limit);
-
-		$qr = $this->database->prepare('SELECT s.*
-			FROM system AS s
-			' . $formatWhere .'
-			' . $formatOrder .'
-			' . $formatLimit
-		);
-
-		foreach($where AS $v) {
-			if (is_array($v)) {
-				foreach ($v as $p) {
-					$valuesArray[] = $p;
-				}
-			} else {
-				$valuesArray[] = $v;
-			}
-		}
-
-		if (empty($valuesArray)) {
-			$qr->execute();
-		} else {
-			$qr->execute($valuesArray);
-		}
-
-		$aw = $qr->fetchAll();
-		$qr->closeCursor();
-
-		foreach ($aw AS $s) {
-			$system = new System();
-			$system->id = $s['id'];
-			$system->rSector = $s['rSector'];
-			$system->rColor = $s['rColor'];
-			$system->xPosition = $s['xPosition'];
-			$system->yPosition = $s['yPosition'];
-			$system->typeOfSystem = $s['typeOfSystem'];
-
-			$this->_Add($system);
-		}
-	}
-
-	public function save() {
-		$systems = $this->_Save();
-
-		foreach ($systems AS $s) {
-			$qr = $this->database->prepare('UPDATE system
-				SET	rColor = ?
-				WHERE id = ?');
-			$qr->execute(array(
-				$s->rColor,
-				$s->id
-			));
-		}
+	/**
+	 * @param int $id
+	 * @return System
+	 */
+	public function get($id)
+	{
+		return $this->entityManager->getRepository(System::class)->get($id);
 	}
 }
