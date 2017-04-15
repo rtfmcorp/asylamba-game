a91e9938619c0f4d6bd6991463270a1ed40a5e0c
Axel Venet <kern046@gmail.com>
2017-04-15 15:05:36 +0200

    Fix global configuration about date and ini parameters

--- system/config/app.config.global.php
+++ system/config/app.config.global.php
@@ -1,15 +1,4 @@
 <?php
-# rÃ©glage temporel et niveau d'erreur
-error_reporting(E_ALL | E_STRICT);
-date_default_timezone_set('Europe/Zurich');
-#	ini_set('memory_limit', '512M');
-	ini_set('memory_limit', '-1');
-	ini_set('error_prepend_string', 'System Error #');
-	ini_set('error_log', 'public/log/php/' . date('Y-m-d') . '.log');
-
-# dÃ©marrage de la session
-session_set_cookie_params(0);
-ini_set('session.gc_maxlifetime', 3600 * 24);
 
 define('FACEBOOK_LINK',			'https://www.facebook.com/asylamba');
 define('GOOGLE_PLUS_LINK',		'https://plus.google.com/+Asylamba-game');
