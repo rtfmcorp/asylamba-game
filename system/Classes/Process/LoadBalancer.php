cf78a40645c42b4714d35d4c7ae03ceffdff22b3
Axel Venet <kern046@gmail.com>
2017-04-29 14:28:16 +0200

    Implement laod balancing manager to diffuse tasks to processes

---
+++ system/Classes/Process/LoadBalancer.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Asylamba\Classes\Process;
+
+use Asylamba\Classes\Task\Task;
+use Asylamba\Classes\DependencyInjection\Container;
+
+class LoadBalancer
+{
+	/** @var Container **/
+	protected $container;
+    /** @var int **/
+    protected $statsVolume;
+    /** @var array **/
+    protected $stats = [];
+    
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+		$this->container = $container;
+		$this->statsVolume = $container->getParameter('worker_stats_volume');
+    }
+    
+	/**
+	 * @param Task $task
+	 */
+    public function affectTask(Task $task)
+    {
+        $this->estimateTime($task);
+        
+        $laziestProcess = $minTime = null;
+		
+        $processManager = $this->container->get('process_manager');
+        
+        foreach ($processManager->getProcesses() as $process) {
+			if ($process->getExpectedWorkTime() < $minTime || $minTime === null) {
+				$laziestProcess = $process;
+				$minTime = $process->getExpectedWorkTime();
+			}
+        }
+        $processManager->affectTask($laziestProcess, $task);
+    }
+    
+	/**
+	 * @param Task $task
+	 */
+    public function storeStats(Task $task)
+    {
+        $key = $task->getManager() . '.' . $task->getMethod();
+        $this->stats[$key][] = $task->getTime();
+        
+        if (count($this->stats[$key]) > $this->statsVolume) {
+            array_shift($this->stats[$key]);
+        }
+    }
+    
+    /**
+     * @param Task $task
+     */
+    public function estimateTime(Task $task)
+    {
+		$key = $task->getManager() . '.' . $task->getMethod();
+		
+		if (!isset($this->stats[$key])) {
+			$task->setEstimatedTime(1.0);
+			return;
+		}
+		
+        $taskStats = $this->stats[$key];
+        $task->setEstimatedTime(array_sum($taskStats) / count($taskStats));
+    }
+}
