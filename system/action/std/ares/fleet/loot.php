4021d5856eaeaff86e91c536847629f382a2de9c
Axel Venet <kern046@gmail.com>
2017-05-15 01:24:52 +0200

    Fix looting on another player

--- system/action/std/ares/fleet/loot.php
+++ system/action/std/ares/fleet/loot.php
@@ -94,6 +94,7 @@ if ($commanderId !== FALSE AND $placeId !== FALSE) {
 
 					if ($commander->getPev() > 0) {
 						$sector = $sectorManager->get($place->rSector);
+						$sectorColor = $colorManager->get($sector->rColor);
 
 						$isFactionSector = ($sector->rColor == $commander->playerColor || $sectorColor->colorLink[$session->get('playerInfo')->get('color')] == Color::ALLY) ? TRUE : FALSE;
 						
