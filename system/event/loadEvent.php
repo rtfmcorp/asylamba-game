60eadefc25d645298c5467caa30543a9578c3010
Axel Venet <kern046@gmail.com>
2017-05-15 01:21:51 +0200

    Remove the session handling of incoming attacks

--- system/event/loadEvent.php
+++ system/event/loadEvent.php
@@ -22,44 +22,6 @@ if (Utils::interval($session->get('lastUpdate')->get('event'), Utils::now(), 's'
 	for ($i = 0; $i < $session->get('playerBase')->get('ms')->size(); $i++) {
 		$places[] = $session->get('playerBase')->get('ms')->get($i)->get('id');
 	}
-
-	$commanders = $commanderManager->getIncomingAttacks($places);
-
-	# ajout des bases des ennemis dans le tableau
-	foreach ($commanders as $commander) {
-		$places[] = $commander->getRBase();
-	}
-	$placeManager->getByIds($places);
-
-	# enlève du controller tous les évents d'attaques entrantes avant de mettre les nouvelles
-	$size = $session->get('playerEvent')->size();
-	for ($i = 0; $i < $size; $i++) {
-		if ($session->get('playerEvent')->get($i)->get('eventType') == EVENT_INCOMING_ATTACK) {
-			$session->get('playerEvent')->remove($i);
-			$i--;
-			$size--;
-		}
-	}
-	
-	foreach ($commanders as $commander) { 
-		# va chercher les heures auxquelles il rentre dans les cercles d'espionnage
-		$startPlace = $placeManager->get($commander->getRBase());
-		$destinationPlace = $placeManager->get($commander->getRPlaceDestination());
-		$times = Game::getAntiSpyEntryTime($startPlace, $destinationPlace, $commander->getArrivalDate());
-
-		if (strtotime(Utils::now()) >= strtotime($times[0])) {
-			$info = $commanderManager->getEventInfo($commander);
-			$info->add('inCircle', $times);
-
-			# ajout de l'événement
-			$session->get('playerEvent')->add(
-				$commander->getArrivalDate(), 
-				EVENT_INCOMING_ATTACK, 
-				$commander->getId(),
-				$info
-			);
-		}
-	}
 	# mettre à jour dLastActivity
 	$player = $playerManager->get($session->get('playerId'));
 	$player->setDLastActivity(Utils::now());
