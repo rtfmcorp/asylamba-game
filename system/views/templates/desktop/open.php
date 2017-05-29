0cbb7b68545dfb5444a13dbc1b8f5a66409e7aa4
acknowledge <jack_ett8@hotmail.com>
2017-05-29 22:19:27 +0200

    Update ganalytics tag for s14

--- system/views/templates/desktop/open.php
+++ system/views/templates/desktop/open.php
@@ -48,7 +48,7 @@ if (ANALYTICS) {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-42636532-11', 'auto');
+  ga('create', 'UA-42636532-13', 'auto');
   ga('send', 'pageview');
 
 </script>
