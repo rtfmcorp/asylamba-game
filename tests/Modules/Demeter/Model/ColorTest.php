22125b405f96ef1f0bedb74f86e3b2ee74bab1af
Jean Liador <Jean.lousberg@yahoo.fr>
2017-06-08 16:49:14 +0200

    Test unitaire Color v1

---
+++ tests/Modules/Demeter/Model/ColorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Asylamba\Modules\Demeter\Model;
+
+use Asylamba\Modules\Demeter\Model\Color;
+
+class CommanderTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEntity()
+    {
+
+
+        $color =
+            new Color();
+
+    }
+
+    public function testIncreseCredit()
+    {
+      $color = new color();
+      $color->credits=1000;
+      $this->assertEquals(2234, $color->increaseCredit(1324));
+    }
+
+    public function testDecreaseCredit()
+    {
+      $color = new color();
+      $color->credits=1000;
+      $this->assertEquals(505, $color->decreaseCredit(495));
+    }
+}
