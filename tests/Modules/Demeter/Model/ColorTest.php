d438e742955bd43251a919aad2b701cc3667f042
Jean Liador <Jean.lousberg@yahoo.fr>
2017-06-29 15:35:33 +0200

    ColorTest fix 1 29/06/2017

--- tests/Modules/Demeter/Model/ColorTest.php
+++ tests/Modules/Demeter/Model/ColorTest.php
@@ -9,23 +9,51 @@ class ColorTest extends \PHPUnit\Framework\TestCase
     public function testEntity()
     {
 
-
         $color =
-            new Color();
+            (new Color())
+            ->setId(1)
+            -setAlive(TRUE)
+            ->setisWinner(FALSE)
+            ->setCredits(1000)
+            //->setPlayers()
+            //->setActivePlayers()
+            ->setRankingPoints(820)
+            ->setPoints(1200)
+            //->setSectors()
+            //->setElectionStatement()
+            ->setIsClosed(TRUE);
+            ->setDescription("Descrip")
+            ->setDClaimVictory(TRUE)
+            ->setDLastElection('2017-05-06 21:20:30')
+            ->setIsInGame(TRUE)
+            ;
 
+            $this->assertEquals(1, $color->getId());
+            $this->assertEquals(TRUE, $color->getAlive());
+            $this->assertEquals(FALSE, $color->getIsWinner());
+            $this->assertEquals(1000, $color->getCredits());
+            //$this->assertEquals(, $color->getPlayers());
+            //$this->assertEquals(, $color->getActivePlayers());
+            $this->assertEquals(820, $color->getRankingPoints());
+            $this->assertEquals(1200, $color->getPoints());
+            //$this->assertEquals(, $color->getSectors());
+            //$this->assertEquals(, $color->getElectionStatement());
+            $this->assertEquals(TRUE, $color->getIsClosed());
+            $this->assertEquals("Descrip", $color->getDescription());
+            $this->assertEquals(TRUE, $color->getDClaimVictory());
+            $this->assertEquals('2017-05-06 21:20:30', $color->getDLastElection());
+            $this->assertEquals(TRUE, $color->getIsInGame());
     }
 
     public function testIncreaseCredit()
     {
       $color = new color();
-      $color->credits=1000;
       $this->assertEquals(2234, $color->increaseCredit(1324));
     }
 
     public function testDecreaseCredit()
     {
       $color = new color();
-      $color->credits=1000;
       $this->assertEquals(505, $color->decreaseCredit(495));
     }
 }
