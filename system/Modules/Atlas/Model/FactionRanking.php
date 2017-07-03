cf6df4190488c7a110b95aca2f8c2a7a02785779
Axel Venet <kern046@gmail.com>
2017-07-04 00:37:06 +0200

    Implement setters and getters for faction ranking

--- system/Modules/Atlas/Model/FactionRanking.php
+++ system/Modules/Atlas/Model/FactionRanking.php
@@ -37,9 +37,311 @@ class FactionRanking {
 	public $territorial; 			# sectors owned
 	public $territorialPosition;
 	public $territorialVariation;
+	
+	/**
+	 * @param int $id
+	 * @return FactionRanking
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+		
+		return $this;
+	}
 
-	public function getId() { return $this->id; }
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{ 
+		return $this->id;
+	}
 
+	/**
+	 * @param int $rankingId
+	 * @return FactionRanking
+	 */
+	public function setRankingId($rankingId)
+	{
+		$this->rRanking = $rankingId;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getRankingId()
+	{
+		return $this->rRanking;
+	}
+	
+	/**
+	 * @param int $factionId
+	 * @return FactionRanking
+	 */
+	public function setFactionId($factionId)
+	{
+		$this->rFaction = $factionId;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getFactionId()
+	{
+		return $this->rFaction;
+	}
+	
+	/**
+	 * @param int $points
+	 * @return FactionRanking
+	 */
+	public function setPoints($points)
+	{
+		$this->points = $points;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getPoints()
+	{
+		return $this->points;
+	}
+	
+	/**
+	 * @param int $pointsPosition
+	 * @return FactionRanking
+	 */
+	public function setPointsPosition($pointsPosition)
+	{
+		$this->pointsPosition = $pointsPosition;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getPointsPosition()
+	{
+		return $this->pointsPosition;
+	}
+	
+	/**
+	 * @param int $pointsVariation
+	 * @return FactionRanking
+	 */
+	public function setPointsVariation($pointsVariation)
+	{
+		$this->pointsVariation = $pointsVariation;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getPointsVariation()
+	{
+		return $this->pointsVariation;
+	}
+	
+	/**
+	 * @param int $newPoints
+	 * @return FactionRanking
+	 */
+	public function setNewPoints($newPoints)
+	{
+		$this->newPoints = $newPoints;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getNewPoints()
+	{
+		return $this->newPoints;
+	}
+	
+	/**
+	 * @param int $general
+	 * @return FactionRanking
+	 */
+	public function setGeneral($general)
+	{
+		$this->general = $general;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getGeneral()
+	{
+		return $this->general;
+	}
+	
+	/**
+	 * @param int $generalPosition
+	 * @return FactionRanking
+	 */
+	public function setGeneralPosition($generalPosition)
+	{
+		$this->generalPosition = $generalPosition;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getGeneralPosition()
+	{
+		return $this->generalPosition;
+	}
+	
+	/**
+	 * @param int $generalVariation
+	 * @return FactionRanking
+	 */
+	public function setGeneralVariation($generalVariation)
+	{
+		$this->generalVariation = $generalVariation;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getGeneralVariation()
+	{
+		return $this->generalVariation;
+	}
+	
+	/**
+	 * @param int $wealth
+	 * @return FactionRanking
+	 */
+	public function setWealth($wealth)
+	{
+		$this->wealth = $wealth;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getWealth()
+	{
+		return $this->wealth;
+	}
+	
+	/**
+	 * @param int $wealthPosition
+	 * @return FactionRanking
+	 */
+	public function setWealthPosition($wealthPosition)
+	{
+		$this->wealthPosition = $wealthPosition;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getWealthPosition()
+	{
+		return $this->wealthPosition;
+	}
+	
+	/**
+	 * @param int $wealthVariation
+	 * @return FactionRanking
+	 */
+	public function setWealthVariation($wealthVariation)
+	{
+		$this->wealthVariation = $wealthVariation;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getWealthVariation()
+	{
+		return $this->wealthVariation;
+	}
+	
+	/**
+	 * @param int $territorial
+	 * @return FactionRanking
+	 */
+	public function setTerritorial($territorial)
+	{
+		$this->territorial = $territorial;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getTerritorial()
+	{
+		return $this->territorial;
+	}
+	
+	/**
+	 * @param int $territorialPosition
+	 * @return FactionRanking
+	 */
+	public function setTerritorialPosition($territorialPosition)
+	{
+		$this->territorialPosition = $territorialPosition;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getTerritorialPosition()
+	{
+		return $this->territorialPosition;
+	}
+	
+	/**
+	 * @param int $territorialVariation
+	 * @return FactionRanking
+	 */
+	public function setTerritorialVariation($territorialVariation)
+	{
+		$this->territorialVariation = $territorialVariation;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getTerritorialVariation()
+	{
+		return $this->territorialVariation;
+	}
+	
 	public function commonRender($playerInfo, $type = 'general') {
 		$r = '';
 
