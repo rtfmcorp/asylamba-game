f2b5e184cf65128b00fcf52d6768034a16746991
Axel Venet <kern046@gmail.com>
2017-11-01 03:12:33 +0100

    Display the current version modifications in project overview template

--- system/views/components/project/overview.php
+++ system/views/components/project/overview.php
@@ -1,10 +1,30 @@
-<?php include COMPONENT . 'project/infos.php'; ?>
-<div class="component size3 news">
+<?php
+
+use ChangelogParser\Manager\ChangelogManager;
+
+include COMPONENT . 'project/infos.php'; 
+
+$changelogManager = new ChangelogManager();
+
+$lastVersion = json_decode($changelogManager->getLastVersion(
+    $this->getContainer()->getParameter('root_path') . '/CHANGELOG.fr.md'
+), true);
+$versionName = key($lastVersion);
+?>
+<div class="component size2">
     <div class="head skin-2">
-        <h2>Vue d'ensemble</h2>
+        <h2>Version actuelle<?= ($versionName !== 'prochainement') ? ": $versionName - {$lastVersion[$versionName]['date']}" : '' ?></h2>
     </div>
     <div class="fix-body">
         <div class="body">
+            <?php foreach ($lastVersion[$versionName]['items'] as $label => $items) { ?>
+                <h3><?= $label ?></h3>
+                <ul>
+                <?php foreach ($items as $item) { ?>
+                    <li><?= $item ?></li>
+                <?php } ?>
+                </ul>
+            <?php } ?>
         </div>
     </div>
 </div>
