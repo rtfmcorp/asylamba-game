ee9556dc516a8442b5c36b130563df9732ec24ce
Axel Venet <avenet@wamiz.com>
2022-01-08 00:08:06 +0100

    Enable the actions scheduling at server start with a dedicated event listener

--- system/Classes/Kernel/ApplicationKernel.php
+++ system/Classes/Kernel/ApplicationKernel.php
@@ -5,9 +5,11 @@ namespace Asylamba\Classes\Kernel;
 use Asylamba\Classes\Daemon\Server;
 use Asylamba\Classes\Database\Database;
 use Asylamba\Classes\Entity\EntityManager;
+use Asylamba\Classes\Event\ServerInitEvent;
 use Asylamba\Classes\Scheduler\CyclicActionScheduler;
 use Asylamba\Modules\Gaia\Manager\SectorManager;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ApplicationKernel extends Kernel
 {
@@ -32,6 +34,10 @@ class ApplicationKernel extends Kernel
 		$this->container->get(EntityManager::class)->init();
 		$this->container->get(CyclicActionScheduler::class)->init();
         $this->container->get(SectorManager::class)->initOwnershipData();
+
+		/** @var EventDispatcherInterface $eventDispatcher */
+		$eventDispatcher = $this->container->get(EventDispatcherInterface::class);
+		$eventDispatcher->dispatch(new ServerInitEvent(), ServerInitEvent::NAME);
 
 		$server = $this->container->get(Server::class);
 		$server->createHttpServer();
